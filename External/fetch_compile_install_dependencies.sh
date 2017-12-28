3033791fd1e3c60b4bd4ddedaaf965194f5e460b
xma <xma@spaceapplications.com>
2017-12-28 16:08:21 +0100

    Fix fix fix in the dependency compiler script

--- External/fetch_compile_install_dependencies.sh
+++ External/fetch_compile_install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #xma@spaceapplications.com
 #This file fetches the dependencies in /Externals, builds and installs them.
-# Version 1.0
+# Version 1.5
 
 #exit immediately if a simple command exits with a nonzero exit value.
 set -e
@@ -19,34 +19,119 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 BUILD_DIR=$DIR"/build"
 INSTALL_DIR=$DIR"/install"
 PKG_DIR=$DIR"/package"
+cmake=false
+boost=false
+eigen=false
+flann=false
+qhull=false
+tinyxml2=false
+yamlcpp=false
+vtk=false
+opencv=false
+pcl=false
 
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied, 
-    using 
-BUILD directory :$BUILD_DIR 
-INSTALL directory :$INSTALL_DIR 	
-"
-  else
-	if [ "$#" -ne 2 ]; then	
-	    echo "Usage: script.sh [BUILD_DIR] [INSTALL_DIR]"
-	else
-	    BUILD_DIR=$1
-    	    INSTALL_DIR=$2
-	fi
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+function show_help {
+
+  cat <<EOF
+\`./bootstrap.sh' prepares Boost for building on a few kinds of systems.
+
+Usage: $0 [OPTION]... 
+
+Defaults for the options are specified in brackets.
+
+Configuration:
+  -h, ?               	    Display this help and exit
+  -s LIB                    Only compile LIB
+			    can be used multiple times (-s LIB1 -s LIB2)
+                            [LIB: cmake boost eigen flann qhull tinyxml2
+			    yamlccp vtk opencv pcl]
+
+Installation directories:
+  -b DIR            	    Build all libraries in DIR
+                            [$BUILD_DIR]
+  -i DIR            	    Install all libraries in DIR
+                            [$INSTALL_DIR]
+  -p DIR            	    Directory where packages are stored
+                            [$PKG_DIR]
+
+EOF
+}
+
+function show_configuration {
+if ($BUILD_ALL) ;then
+	cmake=true
+	boost=true
+	eigen=true
+	flann=true
+	qhull=true
+	tinyxml2=true
+	yamlcpp=true
+	vtk=true
+	opencv=true
+	pcl=true
 fi
+	echo "build directory    = ${BUILD_DIR}"
+	echo "install directory  = ${INSTALL_DIR}"
+	echo "Packages directory = ${PKG_DIR}"
+	echo cmake=$cmake
+	echo boost=$boost
+	echo eigen=$eigen
+	echo flann=$flann
+	echo qhull=$qhull
+	echo tinyxml2=$tinyxml2
+	echo yamlcpp=$yamlcpp
+	echo vtk=$vtk
+	echo opencv=$opencv
+	echo pcl=$pcl
+}
+
+function build {
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+if ( $cmake ); then
+	install_cmake
+fi
+if ( $boost ); then
+	install_boost
+fi
+if ( $eigen ); then
+	install_eigen
+fi
+if ( $flann ); then
+	install_flann
+fi
+if ( $qhull ); then
+	install_qhull
+fi
+if ( $tinyxml2 ); then
+	install_tinyxml2
+fi
+if ( $yamlcpp ); then
+	install_yamlcpp
+fi
+if ( $vtk ); then
+	install_vtk
+fi
+if ( $opencv ); then
+	install_opencv
+fi
+if ( $pcl ); then
+	install_pcl
+fi
+}
 
 #update submodules
 #git submodule update --remote --recursive --depth 1
 
 # if exist remove previous packages
 
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
-
 function install_function {
 if (command -v checkinstall); then
-   sudo checkinstall -y --pakdir $PKG_DIR --nodoc
+   checkinstall -y --pakdir $PKG_DIR --nodoc
 else
    make install
 fi
@@ -54,126 +139,189 @@ fi
 
 function install_cmake {
 if [ ! -d "$INSTALL_DIR" ]; then # should test for 3.10 version > installed
-	echo "Installing Cmake"
+	echo "Installing CMake"
 	wget https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz
 	tar xf cmake-3.10.1.tar.gz
 	cd cmake-3.10.1
 	./bootstrap --prefix=$INSTALL_DIR 
 	make
 	install_function
-	echo "Done."
+	echo "CMake installation Done."
 fi
 }
 
 function install_boost {
 if [[ ! -d "$INSTALL_DIR/include/boost" ]]; then
+	echo "Installing boost"        
 	mkdir -p $BUILD_DIR/boost
 	cd $DIR/boost 
+	echo "boost boostrap"
 	./bootstrap.sh --prefix=$INSTALL_DIR 
-	./b2 --build-dir=$BUILD_DIR/boost 
-	./b2 install 
-	./b2 --clean-all	
+	echo "b2 install"
+	./b2 --build-dir=$BUILD_DIR/boost --prefix=$INSTALL_DIR --NO_COMPRESSION --without-python install
+	echo "clean-all"
+	./b2 --clean-all
+	echo "boost installation Done."
 fi
 }
 
 function install_eigen {
 if [[ ! -n $(find $DIR/package/ -name 'eigen*') ]]; then
+	echo "Installing eigen"
 	mkdir -p $BUILD_DIR/eigen
 	cd $BUILD_DIR/eigen 
 	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR $DIR/eigen 
 	install_function
+	echo "eigen installation Done."
 fi
 }
 
 function install_flann {
 if [[ ! -n $(find $DIR/package/ -name 'flann*') ]]; then
+	echo "Installing flann"
 	mkdir -p $BUILD_DIR/flann
 	cd $BUILD_DIR/flann 
-	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR $DIR/flann -D BUILD_TESTS=OFF -D BUILD_EXAMPLES=OFF 
+	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR -D BUILD_TESTS=OFF -D BUILD_EXAMPLES=OFF $DIR/flann
 	make
 	install_function
+	echo "flann installation Done."
 fi
 }
 
 function install_opencv {
 if [[ ! -n $(find $DIR/package/ -name 'opencv*') ]]; then
+	echo "Installing opencv"
 	mkdir -p $BUILD_DIR/opencv3
 	cd $BUILD_DIR/opencv3
-	cmake -D CMAKE_BUILD_TYPE=RELEASE -D WITH_FFMPEG=OFF -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR $DIR/opencv -D BUILD_DOCS=OFF -D BUILD_EXAMPLES=OFF -D BUILD_TESTS=OFF -D ENABLE_CXX11=ON -D ENABLE_FAST_MATH=ON
+	cmake -D CMAKE_BUILD_TYPE=RELEASE -D WITH_FFMPEG=OFF -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR -D BUILD_DOCS=OFF -D BUILD_EXAMPLES=OFF -D BUILD_TESTS=OFF -D ENABLE_CXX11=ON -D ENABLE_FAST_MATH=ON $DIR/opencv
 	make
 	install_function
+	echo "opencv installation Done."
 fi
 }
 
 function install_pcl {
 if [[ ! -n $(find $DIR/package/ -name 'pcl*') ]]; then
+	echo "Installing pcl"
 	mkdir -p $BUILD_DIR/pcl
 	cd $BUILD_DIR/pcl 
 	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR $DIR/pcl 
 	make
 	install_function
+	echo "pcl installation Done."
 fi
 }
 
 function install_qhull {
 if [[ ! -n $(find $DIR/package/ -name 'qhull*') ]]; then
+	echo "Installing qhull"
 	mkdir -p $BUILD_DIR/qhull
 	cd $BUILD_DIR/qhull 
 	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR $DIR/qhull 
 	make
 	install_function
+	echo "qhull installation Done."
 fi
 }
 
 function install_tinyxml2 {
 if [[ ! -n $(find $DIR/package/ -name 'tinyxml2*') ]]; then
+	echo "Installing tinyxml2"
 	mkdir -p $BUILD_DIR/tinyxml2
 	cd $BUILD_DIR/tinyxml2 
-	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR $DIR/tinyxml2 -D BUILD_TESTING=OFF -D BUILD_TESTS=OFF
+	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR -D BUILD_TESTING=OFF -D BUILD_TESTS=OFF $DIR/tinyxml2
 	install_function
+	echo "tinyxml2 installation Done."
 fi
 }
 
 function install_vtk {
 if [[ ! -n $(find $DIR/package/ -name 'vtk*') ]]; then
+	echo "Installing vtk"
 	mkdir -p $BUILD_DIR/vtk
 	cd $BUILD_DIR/vtk 
-	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR $DIR/vtk -D BUILD_TESTING=OFF
+	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR -D BUILD_TESTING=OFF $DIR/vtk
 	make
 	install_function
+	echo "vtk installation Done."
 fi
 }
 
 function install_yamlcpp {
 if [[ ! -n $(find $DIR/package/ -name 'yamlcpp*') ]]; then
+	echo "Installing yamlcpp"
 	mkdir -p $BUILD_DIR/yamlcpp
 	cd $BUILD_DIR/yamlcpp 
 	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR $DIR/yamlcpp 
 	make
 	install_function
+	echo "yamlcpp installation Done."
 fi
 }
 
-# execute in this order to fix dependencies
- install_cmake
- install_boost
- install_eigen
- install_flann
- install_qhull
- install_tinyxml2
- install_yamlcpp
- install_vtk
- install_opencv
- install_pcl
+BUILD_ALL=true
 
-# delete but install directory 
-
-
-
-
-
-
-
+while getopts "ab:i:p:s:" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    a) 
+	echo "-a was triggered, Parameter: $OPTARG" >&2
+	;;
+    b)  
+	BUILD_DIR=$OPTARG
+        ;;
+    i)  
+	INSTALL_DIR=$OPTARG
+        ;;
+    p)  
+	PKG_DIR=$OPTARG
+        ;;
+    s)  
+	BUILD_ALL=false
+	if [ "cmake" == "$OPTARG" ]; then
+		cmake=true
+	fi
+	if [ "boost" == "$OPTARG" ]; then
+		boost=true
+	fi
+	if [ "eigen" == "$OPTARG" ]; then
+		eigen=true
+	fi
+	if [ "flann" == "$OPTARG" ]; then
+		flann=true
+	fi
+	if [ "qhull" == "$OPTARG" ]; then
+		qhull=true
+	fi
+	if [ "tinyxml2" == "$OPTARG" ]; then
+		tinyxml2=true
+	fi
+	if [ "yamlcpp" == "$OPTARG" ]; then
+		yamlcpp=true
+	fi
+	if [ "vtk" == "$OPTARG" ]; then
+		vtk=true
+	fi
+	if [ "opencv" == "$OPTARG" ]; then
+		opencv=true
+	fi
+	if [ "pcl" == "$OPTARG" ]; then
+		pcl=true
+	fi
+	;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+    esac
+done
+shift $((OPTIND-1))
+[ "$1" = "--" ] && shift
+show_configuration
+build
 
 
 
