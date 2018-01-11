0994892a804a2a33a7a9c537eebdb69ed1010552
xma <xma@spaceapplications.com>
2018-01-11 17:24:12 +0100

    Of the day

--- Tools/Docker/fetch_compile_install_dependencies.sh
+++ Tools/Docker/fetch_compile_install_dependencies.sh
@@ -96,8 +96,8 @@ cd $BUILD_DIR
 [[ ":$PKG_CONFIG_PATH:" != *":$INSTALL_DIR/lib/pkgconfig:"* ]] && PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:$INSTALL_DIR/lib/pkgconfig"
 # add bin install dir to path if not present
 [[ ":$PATH:" != *"$INSTALL_DIR/bin"* ]] && PATH="${PATH}:$INSTALL_DIR/bin"
-export PKG_CONFIG_PATH
-export PATH
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH
+export PATH=$PATH
 
 if ( $cmake ); then
 	install_cmake
