33919e8e328f5ea2cd791cec798c23215f473824
Alessandro Bianco <alessandro.bianco@strath.ac.uk>
2018-05-03 15:54:11 +0100

    Adding Test

---
+++ Tests/InFuseTests/DFNsIntegration/Odometry3D/CorrectLocalizationTester.cpp
@@ -0,0 +1,418 @@
+/* --------------------------------------------------------------------------
+*
+* (C) Copyright …
+*
+* ---------------------------------------------------------------------------
+*/
+
+/*!
+ * @file CorrectLocalizationTester.cpp
+ * @date 03/04/2018
+ * @author Alessandro Bianco
+ */
+
+/*!
+ * @addtogroup GuiTests
+ * 
+ * Implementation of the CorrectLocalizationTester class.
+ * 
+ * 
+ * @{
+ */
+
+
+/* --------------------------------------------------------------------------
+ *
+ * Includes
+ *
+ * --------------------------------------------------------------------------
+ */
+#include "CorrectLocalizationTester.hpp"
+#include<pcl/io/ply_io.h>
+
+using namespace dfn_ci;
+using namespace Converters;
+using namespace VisualPointFeatureVector3DWrapper;
+using namespace PointCloudWrapper;
+using namespace Common;
+using namespace PoseWrapper;
+using namespace SupportTypes;
+
+#define DELETE_IF_NOT_NULL(pointer) \
+	if (pointer != NULL) \
+		{ \
+		delete(pointer); \
+		}
+
+/* --------------------------------------------------------------------------
+ *
+ * Public Member Functions
+ *
+ * --------------------------------------------------------------------------
+ */
+CorrectLocalizationTester::CorrectLocalizationTester() 
+	{
+	extractorConfigurationFile = "";
+	descriptorConfigurationFile = "";
+	matcherConfigurationFile = "";
+	
+	modelCloudFilePath = "";
+	sceneCloudFilePath = "";
+
+	extractor = NULL;
+	descriptor = NULL;
+	matcher = NULL;
+
+	inputSceneCloud = NULL;
+	inputModelCloud = NULL;
+	inputTruthModelPoseInScene = NULL;
+	sceneKeypointsVector = NULL;
+	modelKeypointsVector = NULL;
+	sceneFeaturesVector = NULL;
+	modelFeaturesVector = NULL;
+	outputModelPoseInScene = NULL;
+	
+	outputMatcherSuccess = false;
+	dfnsWereConfigured = false;
+	inputsWereLoaded = false;
+	groundTruthWasLoaded = false;
+
+	SetUpMocksAndStubs();
+	}
+
+CorrectLocalizationTester::~CorrectLocalizationTester()
+	{
+	delete(stubCloudCache);
+	delete(mockCloudConverter);
+
+	delete(stubInverseCloudCache);
+	delete(mockInverseCloudConverter);
+
+	delete(stubVector3dCache);
+	delete(mockVector3dConverter);
+
+	delete(stubNormalsCache);
+	delete(mockNormalsConverter);
+
+	delete(stubFeaturesCloudCache);
+	delete(mockFeaturesCloudConverter);
+
+	delete(stubTransformCache);
+	delete(mockTransformConverter);
+	
+	DELETE_IF_NOT_NULL(inputSceneCloud);
+	DELETE_IF_NOT_NULL(inputModelCloud);
+	DELETE_IF_NOT_NULL(inputTruthModelPoseInScene);
+	DELETE_IF_NOT_NULL(sceneKeypointsVector);
+	DELETE_IF_NOT_NULL(modelKeypointsVector);
+	DELETE_IF_NOT_NULL(sceneFeaturesVector);
+	DELETE_IF_NOT_NULL(modelFeaturesVector);
+	DELETE_IF_NOT_NULL(outputModelPoseInScene);
+	}
+
+void CorrectLocalizationTester::SetInputClouds(std::string sceneCloudFilePath, std::string modelCloudFilePath, std::string groundTruthPoseFilePath)
+	{
+	this->sceneCloudFilePath = sceneCloudFilePath;
+	this->modelCloudFilePath = modelCloudFilePath;
+	this->groundTruthPoseFilePath = groundTruthPoseFilePath;
+
+	LoadPointClouds();
+	LoadGroudTruthPose();
+	}
+
+void CorrectLocalizationTester::SetConfigurationFiles(std::string extractorConfigurationFile, std::string descriptorConfigurationFile, std::string matcherConfigurationFile)
+	{
+	this->extractorConfigurationFile = extractorConfigurationFile;
+	this->descriptorConfigurationFile = descriptorConfigurationFile;
+	this->matcherConfigurationFile = matcherConfigurationFile;
+
+	if (extractor != NULL)
+		{
+		ConfigureDfns();
+		}
+	}
+
+void CorrectLocalizationTester::SetDfns(dfn_ci::FeaturesExtraction3DInterface* extractor, dfn_ci::FeaturesDescription3DInterface* descriptor, dfn_ci::FeaturesMatching3DInterface* matcher)
+	{
+	this->extractor = extractor;
+	this->descriptor = descriptor;
+	this->matcher = matcher;
+
+	if (extractorConfigurationFile != "")
+		{
+		ConfigureDfns();
+		}
+	}
+
+void CorrectLocalizationTester::ExecuteDfns()
+	{
+	ASSERT(dfnsWereConfigured, "Error: there was a call to ExecuteDfns before actually configuring the DFNs");
+	ASSERT(inputsWereLoaded && groundTruthWasLoaded, "Error: there was a call to ExecuteDfns before actually loading inputs");	
+
+	ExtractFeatures();
+	DescribeFeatures();
+	MatchFeatures();
+	}
+
+bool CorrectLocalizationTester::IsOutputCorrect(float relativeLocationError, float relativeOrientationError, float absoluteLocationError)
+	{
+	if (!outputMatcherSuccess)
+		{
+		return false;
+		}
+
+	float modelSize = ComputeModelSize();
+	float locationError = ComputeLocationError();
+	float orientationError = ComputeOrientationError(modelSize);
+
+	PRINT_TO_LOG("The ground truth pose is: ", ToString(*inputTruthModelPoseInScene));
+	PRINT_TO_LOG("The model size is: ", modelSize);
+	PRINT_TO_LOG("The location error is: ", locationError);
+	PRINT_TO_LOG("The orientation error is: ", orientationError);
+
+	bool WithinRelativeLocationError = (locationError <= relativeLocationError * modelSize);
+	bool WithinRelatibeOrientationError  = (orientationError <= relativeOrientationError * modelSize);
+	bool WithinAbsoluteLocationError = (locationError <= absoluteLocationError);
+
+	if (!WithinRelativeLocationError)
+		{
+		PRINT_TO_LOG("The location error exceeds the relative Error", relativeLocationError);
+		}
+	if (!WithinRelatibeOrientationError)
+		{
+		PRINT_TO_LOG("The orientation error exceeds the relative Error", relativeOrientationError);
+		}
+	if (!WithinAbsoluteLocationError)
+		{
+		PRINT_TO_LOG("The location error exceeds the absolute Error", absoluteLocationError);
+		}
+
+	return (WithinRelativeLocationError && WithinRelatibeOrientationError && WithinAbsoluteLocationError);
+	}
+
+/* --------------------------------------------------------------------------
+ *
+ * Private Member Functions
+ *
+ * --------------------------------------------------------------------------
+ */
+void CorrectLocalizationTester::ExtractFeatures()
+	{
+	extractor->pointCloudInput(inputSceneCloud);
+	extractor->process();
+
+	DELETE_IF_NOT_NULL(sceneKeypointsVector);
+	sceneKeypointsVector = extractor->featuresSetOutput();
+	PRINT_TO_LOG("Number of scene keypoints extracted is", GetNumberOfPoints(*sceneKeypointsVector));
+
+	extractor->pointCloudInput(inputModelCloud);
+	extractor->process();
+
+	DELETE_IF_NOT_NULL(modelKeypointsVector);
+	modelKeypointsVector = extractor->featuresSetOutput();
+	PRINT_TO_LOG("Number of model keypoints extracted is", GetNumberOfPoints(*modelKeypointsVector));
+	}
+
+void CorrectLocalizationTester::DescribeFeatures()
+	{
+	descriptor->pointCloudInput(inputSceneCloud);
+	descriptor->featuresSetInput(sceneKeypointsVector);
+	descriptor->process();
+
+	DELETE_IF_NOT_NULL(sceneFeaturesVector);
+	sceneFeaturesVector = descriptor->featuresSetWithDescriptorsOutput();
+	PRINT_TO_LOG("Number of scene features described is", GetNumberOfPoints(*sceneFeaturesVector));
+
+	descriptor->pointCloudInput(inputModelCloud);
+	descriptor->featuresSetInput(modelKeypointsVector);
+	descriptor->process();
+
+	DELETE_IF_NOT_NULL(modelFeaturesVector);
+	modelFeaturesVector = descriptor->featuresSetWithDescriptorsOutput();
+	PRINT_TO_LOG("Number of model features described is", GetNumberOfPoints(*modelFeaturesVector));
+	}
+
+void CorrectLocalizationTester::MatchFeatures()
+	{
+	matcher->sourceFeaturesVectorInput(modelFeaturesVector);
+	matcher->sinkFeaturesVectorInput(sceneFeaturesVector);
+	matcher->process();
+
+	DELETE_IF_NOT_NULL(outputModelPoseInScene);
+	outputModelPoseInScene = matcher->transformOutput();
+	outputMatcherSuccess = matcher->successOutput();
+
+	if (outputMatcherSuccess)
+		{
+		PRINT_TO_LOG("Matching was successful, the pose found is:", ToString(*outputModelPoseInScene));
+		}
+	else
+		{
+		PRINT_TO_LOG("Matching was NOT successful", "");
+		}
+	}
+
+void CorrectLocalizationTester::SetUpMocksAndStubs()
+	{
+	stubCloudCache = new Stubs::CacheHandler<pcl::PointCloud<pcl::PointXYZ>::ConstPtr, PointCloudConstPtr>;
+	mockCloudConverter = new Mocks::PclPointCloudToPointCloudConverter();
+	ConversionCache<pcl::PointCloud<pcl::PointXYZ>::ConstPtr, PointCloudConstPtr, PclPointCloudToPointCloudConverter>::Instance(stubCloudCache, mockCloudConverter);
+
+	stubInverseCloudCache = new Stubs::CacheHandler<PointCloudConstPtr, pcl::PointCloud<pcl::PointXYZ>::ConstPtr>;
+	mockInverseCloudConverter = new Mocks::PointCloudToPclPointCloudConverter();
+	ConversionCache<PointCloudConstPtr, pcl::PointCloud<pcl::PointXYZ>::ConstPtr, PointCloudToPclPointCloudConverter>::Instance(stubInverseCloudCache, mockInverseCloudConverter);
+
+	stubVector3dCache = new Stubs::CacheHandler<cv::Mat, VisualPointFeatureVector3DConstPtr>();
+	mockVector3dConverter = new Mocks::MatToVisualPointFeatureVector3DConverter();
+	ConversionCache<cv::Mat, VisualPointFeatureVector3DConstPtr, MatToVisualPointFeatureVector3DConverter>::Instance(stubVector3dCache, mockVector3dConverter);
+
+	stubNormalsCache = new Stubs::CacheHandler<PointCloudConstPtr, pcl::PointCloud<pcl::Normal>::ConstPtr>();
+	mockNormalsConverter = new Mocks::PointCloudToPclNormalsCloudConverter();
+	ConversionCache<PointCloudConstPtr, pcl::PointCloud<pcl::Normal>::ConstPtr, PointCloudToPclNormalsCloudConverter>::Instance(stubNormalsCache, mockNormalsConverter);
+
+	stubFeaturesCloudCache = new Stubs::CacheHandler<VisualPointFeatureVector3DConstPtr, PointCloudWithFeatures>();
+	mockFeaturesCloudConverter = new Mocks::VisualPointFeatureVector3DToPclPointCloudConverter();
+	ConversionCache<VisualPointFeatureVector3DConstPtr, PointCloudWithFeatures, VisualPointFeatureVector3DToPclPointCloudConverter>::Instance(stubFeaturesCloudCache, mockFeaturesCloudConverter);
+
+	stubTransformCache = new Stubs::CacheHandler<Eigen::Matrix4f, Transform3DConstPtr>();
+	mockTransformConverter = new Mocks::EigenTransformToTransform3DConverter();
+	ConversionCache<Eigen::Matrix4f, Transform3DConstPtr, EigenTransformToTransform3DConverter>::Instance(stubTransformCache, mockTransformConverter);
+	}
+
+void CorrectLocalizationTester::LoadPointClouds()
+	{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr baseScenePclCloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZ> >();
+	pcl::io::loadPLYFile(sceneCloudFilePath, *baseScenePclCloud);
+
+	DELETE_IF_NOT_NULL(inputSceneCloud);	
+	inputSceneCloud = pointCloudConverter.Convert(baseScenePclCloud);
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr baseModelPclCloud = boost::make_shared<pcl::PointCloud<pcl::PointXYZ> >();
+	pcl::io::loadPLYFile(modelCloudFilePath, *baseModelPclCloud);
+
+	DELETE_IF_NOT_NULL(inputModelCloud);	
+	inputModelCloud = pointCloudConverter.Convert(baseModelPclCloud);
+
+	inputsWereLoaded = true;
+	}
+
+void CorrectLocalizationTester::LoadGroudTruthPose()
+	{
+	std::ifstream file(groundTruthPoseFilePath.c_str());
+	ASSERT(file.good(), "Error: groundTruthPoseFilePath is invalid");
+
+	float x, y, z, qx, qy, qz, qw;
+	file >> x >> y >> z >> qx >> qy >> qz >> qw;
+
+	Pose3DPtr groundTruthPose = NewPose3D();
+	SetPosition(*groundTruthPose, x, y, z);
+	SetOrientation(*groundTruthPose, qx, qy, qz, qw);
+
+	file.close();
+
+	DELETE_IF_NOT_NULL(inputTruthModelPoseInScene);
+	inputTruthModelPoseInScene = groundTruthPose;
+	groundTruthWasLoaded = true;
+	}
+
+void CorrectLocalizationTester::ConfigureDfns()
+	{
+	ASSERT(extractor != NULL && matcher != NULL, "One mandatory DFN was not set up");
+	ASSERT(extractorConfigurationFile != "" && matcherConfigurationFile != "", "One mandatory DFN configuration file was not set up");
+	ASSERT(descriptor == NULL || descriptorConfigurationFile != "", "Descriptor DFN configuration file was not set up");
+
+	extractor->setConfigurationFile(extractorConfigurationFile);
+	extractor->configure();
+
+	if (descriptor != NULL)
+		{
+		descriptor->setConfigurationFile(descriptorConfigurationFile);
+		descriptor->configure();
+		}
+
+	matcher->setConfigurationFile(matcherConfigurationFile);
+	matcher->configure();
+
+	dfnsWereConfigured = true;
+	}
+
+float CorrectLocalizationTester::ComputeLocationError()
+	{
+	float distanceX = GetXPosition(*outputModelPoseInScene) - GetXPosition(*inputTruthModelPoseInScene);
+	float distanceY = GetYPosition(*outputModelPoseInScene) - GetYPosition(*inputTruthModelPoseInScene);
+	float distanceZ = GetZPosition(*outputModelPoseInScene) - GetZPosition(*inputTruthModelPoseInScene);
+	float squaredDistance = distanceX*distanceX + distanceY*distanceY + distanceZ*distanceZ;
+
+	return std::sqrt(squaredDistance);
+	}
+
+float CorrectLocalizationTester::ComputeOrientationError(float modelSize)
+	{
+	Eigen::Quaternion<float> outputRotation
+		(
+		GetWOrientation(*outputModelPoseInScene), 
+		GetXOrientation(*outputModelPoseInScene), 
+		GetYOrientation(*outputModelPoseInScene), 
+		GetZOrientation(*outputModelPoseInScene)
+		);
+
+	Eigen::Quaternion<float> truthRotation
+		(
+		GetWOrientation(*inputTruthModelPoseInScene), 
+		GetXOrientation(*inputTruthModelPoseInScene), 
+		GetYOrientation(*inputTruthModelPoseInScene), 
+		GetZOrientation(*inputTruthModelPoseInScene)
+		);
+
+	//A point is taken on a sphere of radius modelSize/2; two points are create by rotation this test point by the outputRotation and the truth rotation.
+	Eigen::Vector3f testPoint(modelSize/2, 0, 0);
+	Eigen::Vector3f outputRotatedPoint = outputRotation * testPoint;
+	Eigen::Vector3f truthRotatedPoint = truthRotation * testPoint;
+
+	//The distance between the two points is taken as a measure of the orientation error.
+	float differenceX = outputRotatedPoint.x() - truthRotatedPoint.x();
+	float differenceY = outputRotatedPoint.y() - truthRotatedPoint.y();
+	float differenceZ = outputRotatedPoint.z() - truthRotatedPoint.z();
+	float squaredDistance = differenceX*differenceX + differenceY*differenceY + differenceZ*differenceZ;
+
+	return std::sqrt(squaredDistance);
+	}
+
+float CorrectLocalizationTester::ComputeModelSize()
+	{
+	if ( GetNumberOfPoints(*inputModelCloud) == 0)
+		{
+		return 0;
+		}
+
+	//The size of the model is computed by wrapping the model in a tetrahedron and taking the diagonal.
+	float minX = GetXCoordinate(*inputModelCloud, 0);
+	float minY = GetYCoordinate(*inputModelCloud, 0);
+	float minZ = GetZCoordinate(*inputModelCloud, 0);
+	float maxX = minX;
+	float maxY = minY;
+	float maxZ = minZ;
+
+	for(int pointIndex = 1; pointIndex < GetNumberOfPoints(*inputModelCloud); pointIndex++)
+		{
+		float x = GetXCoordinate(*inputModelCloud, pointIndex);
+		float y = GetYCoordinate(*inputModelCloud, pointIndex);
+		float z = GetZCoordinate(*inputModelCloud, pointIndex);
+
+		minX = (minX > x) ? x : minX;
+		minY = (minY > y) ? y : minY;
+		minZ = (minZ > z) ? z : minZ;
+		maxX = (maxX < x) ? x : maxX;
+		maxY = (maxY < y) ? y : maxY;
+		maxZ = (maxZ < z) ? z : maxZ;
+		}
+
+	float differenceX = maxX - minX;
+	float differenceY = maxY - minY;
+	float differenceZ = maxZ - minZ;
+	float squaredSize = differenceX*differenceX + differenceY*differenceY + differenceZ*differenceZ;
+
+	return std::sqrt(squaredSize);
+	}
+
+/** @} */
---
+++ Tests/InFuseTests/DFNsIntegration/Odometry3D/CorrectLocalizationTester.hpp
@@ -0,0 +1,149 @@
+/* --------------------------------------------------------------------------
+*
+* (C) Copyright …
+*
+* ---------------------------------------------------------------------------
+*/
+
+/*!
+ * @file CorrectLocalizationTester.hpp
+ * @date 03/05/2018
+ * @author Alessandro Bianco
+ */
+
+/*!
+ * @addtogroup GuiTests
+ * 
+ * This class will test requirement 4.1.1.9 of deliverable 5.5.
+ * " The matched model within the scene should be located and oriented within 10% of the size and relative orientation of the model. " and
+ * " Distance between ground truth pose and estimate pose should not be higher than 5% of the operating distance."
+ *
+ * @{
+ */
+
+#ifndef CORRECT_LOCALIZATION_TESTER_HPP
+#define CORRECT_LOCALIZATION_TESTER_HPP
+
+
+/* --------------------------------------------------------------------------
+ *
+ * Includes
+ *
+ * --------------------------------------------------------------------------
+ */
+#include <FeaturesExtraction3D/FeaturesExtraction3DInterface.hpp>
+#include <FeaturesDescription3D/FeaturesDescription3DInterface.hpp>
+#include <FeaturesMatching3D/FeaturesMatching3DInterface.hpp>
+#include <Errors/Assert.hpp>
+
+#include <VisualPointFeatureVector3D.hpp>
+#include <PointCloud.hpp>
+#include <Pose.hpp>
+#include <PclPointCloudToPointCloudConverter.hpp>
+
+#include <ConversionCache/ConversionCache.hpp>
+#include <Stubs/Common/ConversionCache/CacheHandler.hpp>
+#include <Mocks/Common/Converters/PointCloudToPclPointCloudConverter.hpp>
+#include <Mocks/Common/Converters/PclPointCloudToPointCloudConverter.hpp>
+#include <Mocks/Common/Converters/MatToVisualPointFeatureVector3DConverter.hpp>
+#include <Mocks/Common/Converters/EigenTransformToTransform3DConverter.hpp>
+#include <Mocks/Common/Converters/VisualPointFeatureVector3DToPclPointCloudConverter.hpp>
+#include <Mocks/Common/Converters/PointCloudToPclNormalsCloudConverter.hpp>
+
+#include <stdlib.h>
+#include <fstream>
+#include <string>
+#include <vector>
+
+/* --------------------------------------------------------------------------
+ *
+ * Class definition
+ *
+ * --------------------------------------------------------------------------
+ */
+class CorrectLocalizationTester
+	{
+	/* --------------------------------------------------------------------
+	 * Public
+	 * --------------------------------------------------------------------
+	 */
+	public:
+		CorrectLocalizationTester();
+		~CorrectLocalizationTester();
+
+		void SetInputClouds(std::string sceneCloudFilePath, std::string modelCloudFilePath, std::string groundTruthPoseFilePath);
+		void SetConfigurationFiles(std::string extractorConfigurationFile, std::string descriptorConfigurationFile, std::string matcherConfigurationFile);
+		void SetDfns(dfn_ci::FeaturesExtraction3DInterface* extractor, dfn_ci::FeaturesDescription3DInterface* descriptor, dfn_ci::FeaturesMatching3DInterface* matcher);
+
+		void ExecuteDfns();
+		bool IsOutputCorrect(float relativeLocationError, float relativeOrientationError, float absoluteLocationError);
+
+	/* --------------------------------------------------------------------
+	 * Protected
+	 * --------------------------------------------------------------------
+	 */
+	protected:
+
+
+	/* --------------------------------------------------------------------
+	 * Private
+	 * --------------------------------------------------------------------
+	 */
+	private:
+		Stubs::CacheHandler<pcl::PointCloud<pcl::PointXYZ>::ConstPtr, PointCloudWrapper::PointCloudConstPtr>* stubCloudCache;
+		Mocks::PclPointCloudToPointCloudConverter* mockCloudConverter;
+
+		Stubs::CacheHandler<PointCloudWrapper::PointCloudConstPtr, pcl::PointCloud<pcl::PointXYZ>::ConstPtr>* stubInverseCloudCache;
+		Mocks::PointCloudToPclPointCloudConverter* mockInverseCloudConverter;
+
+		Stubs::CacheHandler<cv::Mat, VisualPointFeatureVector3DWrapper::VisualPointFeatureVector3DConstPtr>* stubVector3dCache;
+		Mocks::MatToVisualPointFeatureVector3DConverter* mockVector3dConverter;
+
+		Stubs::CacheHandler<PointCloudWrapper::PointCloudConstPtr, pcl::PointCloud<pcl::Normal>::ConstPtr >* stubNormalsCache;
+		Mocks::PointCloudToPclNormalsCloudConverter* mockNormalsConverter;
+
+		Stubs::CacheHandler<VisualPointFeatureVector3DWrapper::VisualPointFeatureVector3DConstPtr, Converters::SupportTypes::PointCloudWithFeatures >* stubFeaturesCloudCache;
+		Mocks::VisualPointFeatureVector3DToPclPointCloudConverter* mockFeaturesCloudConverter;
+
+		Stubs::CacheHandler<Eigen::Matrix4f, PoseWrapper::Transform3DConstPtr>* stubTransformCache;
+		Mocks::EigenTransformToTransform3DConverter* mockTransformConverter;
+
+		std::string extractorConfigurationFile, descriptorConfigurationFile, matcherConfigurationFile;
+		std::string sceneCloudFilePath, modelCloudFilePath, groundTruthPoseFilePath;
+		dfn_ci::FeaturesExtraction3DInterface* extractor;
+		dfn_ci::FeaturesDescription3DInterface* descriptor;
+		dfn_ci::FeaturesMatching3DInterface* matcher;
+
+		PointCloudWrapper::PointCloudConstPtr inputSceneCloud;
+		PointCloudWrapper::PointCloudConstPtr inputModelCloud;
+		PoseWrapper::Pose3DConstPtr inputTruthModelPoseInScene;
+		VisualPointFeatureVector3DWrapper::VisualPointFeatureVector3DConstPtr sceneKeypointsVector;
+		VisualPointFeatureVector3DWrapper::VisualPointFeatureVector3DConstPtr modelKeypointsVector;
+		VisualPointFeatureVector3DWrapper::VisualPointFeatureVector3DConstPtr sceneFeaturesVector;
+		VisualPointFeatureVector3DWrapper::VisualPointFeatureVector3DConstPtr modelFeaturesVector;
+		PoseWrapper::Pose3DConstPtr outputModelPoseInScene;
+		bool outputMatcherSuccess;
+
+		Converters::PclPointCloudToPointCloudConverter pointCloudConverter;
+		bool dfnsWereConfigured;
+		bool inputsWereLoaded;
+		bool groundTruthWasLoaded;
+
+		void ExtractFeatures();
+		void DescribeFeatures();
+		void MatchFeatures();
+
+		void SetUpMocksAndStubs();
+		void LoadPointClouds();
+		void LoadGroudTruthPose();
+		void ConfigureDfns();
+
+		float ComputeLocationError();
+		float ComputeOrientationError(float modelSize);
+		float ComputeModelSize();
+	};
+
+#endif
+
+/* CorrectLocalizationTester.hpp */
+/** @} */
