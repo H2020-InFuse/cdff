a1d79cfb694b6a2c09d1acb453f82fb43d3d1eeb
Romain Michalec <romain.michalec@strath.ac.uk>
2018-05-11 14:36:09 +0100

    Generate interface class for all 2D feature description DFNs (ORB)
    Generated from its YAML description using CDFF-dev's DFN Template Generator

--- DFNs/FeaturesDescription2D/FeaturesDescription2DInterface.cpp
+++ DFNs/FeaturesDescription2D/FeaturesDescription2DInterface.cpp
@@ -1,74 +1,36 @@
-/* --------------------------------------------------------------------------
-*
-* (C) Copyright …
-*
-* ---------------------------------------------------------------------------
-*/
-
-/*!
- * @file FeaturesDescription2DInterface.cpp
- * @date 21/02/2018
- * @author Alessandro Bianco (with code generation support)
- */
-
-/*!
+/**
  * @addtogroup DFNs
- * 
- * Implementation of the FeaturesDescription2DInterface class
- * 
- * 
  * @{
  */
 
-/* --------------------------------------------------------------------------
- *
- * Includes
- *
- * --------------------------------------------------------------------------
- */
 #include "FeaturesDescription2DInterface.hpp"
-#include "Errors/Assert.hpp"
 
-namespace dfn_ci {
+namespace dfn_ci
+{
 
-using namespace FrameWrapper;
-using namespace VisualPointFeatureVector2DWrapper;
-
-/* --------------------------------------------------------------------------
- *
- * Public Member Functions
- *
- * --------------------------------------------------------------------------
- */
 FeaturesDescription2DInterface::FeaturesDescription2DInterface()
-	{
-	
-	}
+{
+}
 
 FeaturesDescription2DInterface::~FeaturesDescription2DInterface()
-	{
+{
+}
 
-	}
+void FeaturesDescription2DInterface::frameInput(const CTypes::Frame& data)
+{
+    inFrame = data;
+}
 
-void FeaturesDescription2DInterface::imageInput(FrameConstPtr data) 
-	{
-    	inImage = data;
-	}
+void FeaturesDescription2DInterface::featuresInput(const CTypes::VisualPointFeatureVector2D& data)
+{
+    inFeatures = data;
+}
 
-void FeaturesDescription2DInterface::featuresSetInput(VisualPointFeatureVector2DConstPtr data)
-	{
-	inFeaturesSet = data;
-	}
-
-VisualPointFeatureVector2DConstPtr FeaturesDescription2DInterface::featuresSetWithDescriptorsOutput() 
-	{
-    	return outFeaturesSetWithDescriptors;
-	}
-
-
-
+const CTypes::VisualPointFeatureVector2D& FeaturesDescription2DInterface::featuresOutput() const
+{
+    return outFeatures;
+}
 
 }
 
 /** @} */
-
--- DFNs/FeaturesDescription2D/FeaturesDescription2DInterface.hpp
+++ DFNs/FeaturesDescription2D/FeaturesDescription2DInterface.hpp
@@ -1,90 +1,55 @@
-/* --------------------------------------------------------------------------
-*
-* (C) Copyright …
-*
-* --------------------------------------------------------------------------
-*/
-
-/*!
- * @file FeaturesDescription2DInterface.hpp
- * @date 21/02/2018
- * @author Alessandro Bianco (with code generation support)
- */
-
-/*!
+/**
  * @addtogroup DFNs
- * 
- *  This is the common interface of all DFNs that compute a descriptor for a set of 2D points.    
- *
  * @{
  */
-#ifndef FEATURES_DESCRIPTION_2D_INTERFACE_HPP
-#define FEATURES_DESCRIPTION_2D_INTERFACE_HPP
 
-/* --------------------------------------------------------------------------
- *
- * Includes
- *
- * --------------------------------------------------------------------------
- */
-#include <DFNCommonInterface.hpp>
-#include <Frame.hpp>
-#include <VisualPointFeatureVector2D.hpp>
-#include <FrameToMatConverter.hpp>
+#ifndef FEATURESDESCRIPTION2D_INTERFACE_HPP
+#define FEATURESDESCRIPTION2D_INTERFACE_HPP
 
+#include "DFNCommonInterface.hpp"
 
-namespace dfn_ci {
+namespace CTypes {
+#include <VisualPointFeatureVector2D.h>
+#include <Frame.h>
+}
 
-
-/* --------------------------------------------------------------------------
- *
- * Class definition
- *
- * --------------------------------------------------------------------------
- */
+namespace dfn_ci
+{
+    /**
+     * Common interface for all DFNs that compute descriptors for 2D keypoints
+     */
     class FeaturesDescription2DInterface : public DFNCommonInterface
     {
-	/* --------------------------------------------------------------------
-	 * Public
-	 * --------------------------------------------------------------------
-	 */
         public:
+
             FeaturesDescription2DInterface();
             virtual ~FeaturesDescription2DInterface();
-            /**
-            * Send value to input port image
-            * @param image, a 2D image taken from a camera
-            */
-            virtual void imageInput(FrameWrapper::FrameConstPtr data);
 
             /**
-            * Receive value from input port featuresSet
-            * @param featuresSet, This is the set of the points extracted from the image with no associated descriptor.
-            */
-            virtual void featuresSetInput(VisualPointFeatureVector2DWrapper::VisualPointFeatureVector2DConstPtr data);
+             * Send value to input port "frame"
+             * @param frame, 2D image captured by a camera
+             */
+            virtual void frameInput(const CTypes::Frame& data);
+            /**
+             * Send value to input port "features"
+             * @param features, keypoints extracted from the image, without any descriptors
+             */
+            virtual void featuresInput(const CTypes::VisualPointFeatureVector2D& data);
 
             /**
-            * Receive value from output port featuresSetWithDescriptors
-            * @param featuresSetWithDescriptors, This is the set of the points extracted from the image. A descriptor is associated to each point.
-            */
-            virtual VisualPointFeatureVector2DWrapper::VisualPointFeatureVector2DConstPtr featuresSetWithDescriptorsOutput();
+             * Query value from output port "features"
+             * @return features, same keypoints, with added descriptors
+             */
+            virtual const CTypes::VisualPointFeatureVector2D& featuresOutput() const;
 
-	/* --------------------------------------------------------------------
-	 * Protected
-	 * --------------------------------------------------------------------
-	 */
         protected:
-            FrameWrapper::FrameConstPtr inImage;
-            VisualPointFeatureVector2DWrapper::VisualPointFeatureVector2DConstPtr inFeaturesSet;
-            VisualPointFeatureVector2DWrapper::VisualPointFeatureVector2DConstPtr outFeaturesSetWithDescriptors;
 
-	/* --------------------------------------------------------------------
-	 * Private
-	 * --------------------------------------------------------------------
-	 */
-	private:
+            CTypes::Frame inFrame;
+            CTypes::VisualPointFeatureVector2D inFeatures;
+            CTypes::VisualPointFeatureVector2D outFeatures;
     };
 }
-#endif
-/* FeaturesDescription2DInterface.hpp */
+
+#endif // FEATURESDESCRIPTION2D_INTERFACE_HPP
+
 /** @} */
