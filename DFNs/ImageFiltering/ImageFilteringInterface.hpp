1a59f1424e0cd6f0f5787fb2fbc0c4e0059943ac
Romain Michalec <romain.michalec@strath.ac.uk>
2018-05-27 00:23:51 +0100

    Generate interface class for all 2D image filters
    Generated from its YAML description using CDFF-dev's DFN Template Generator

--- DFNs/ImageFiltering/ImageFilteringInterface.hpp
+++ DFNs/ImageFiltering/ImageFilteringInterface.hpp
@@ -1,83 +1,45 @@
-/* --------------------------------------------------------------------------
-*
-* (C) Copyright …
-*
-* --------------------------------------------------------------------------
-*/
-
-/*!
- * @file ImageFilteringInterface.hpp
- * @date 19/02/2018
- * @author Alessandro Bianco (with code generation support)
- */
-
-/*!
+/**
  * @addtogroup DFNs
- * 
- *  This is the common interface of all DFNs that apply some filter to the image.    
- *
  * @{
  */
-#ifndef IMAGE_FILTERING_INTERFACE_HPP
-#define IMAGE_FILTERING_INTERFACE_HPP
 
-/* --------------------------------------------------------------------------
- *
- * Includes
- *
- * --------------------------------------------------------------------------
- */
-#include <DFNCommonInterface.hpp>
-#include <Frame.hpp>
-#include <FrameToMatConverter.hpp>
-#include <MatToFrameConverter.hpp>
+#ifndef IMAGEFILTERING_INTERFACE_HPP
+#define IMAGEFILTERING_INTERFACE_HPP
 
+#include "DFNCommonInterface.hpp"
+#include <Frame.h>
 
-namespace dfn_ci {
-
-
-/* --------------------------------------------------------------------------
- *
- * Class definition
- *
- * --------------------------------------------------------------------------
- */
+namespace dfn_ci
+{
+    /**
+     * DFN that applies an image processing filter to a 2D image
+     */
     class ImageFilteringInterface : public DFNCommonInterface
     {
-	/* --------------------------------------------------------------------
-	 * Public
-	 * --------------------------------------------------------------------
-	 */
         public:
+
             ImageFilteringInterface();
             virtual ~ImageFilteringInterface();
-            /**
-            * Send value to input port image
-            * @param image, a 2D image taken from a camera
-            */
-            virtual void imageInput(FrameWrapper::FrameConstPtr data);
 
             /**
-            * Receive value from output port undistortedImage
-            * @param undistortedImage, the undistorted image output.
-            */
-            virtual FrameWrapper::FrameConstPtr filteredImageOutput();
+             * Send value to input port "image"
+             * @param image: 2D image captured by a camera
+             */
+            virtual void imageInput(const asn1SccFrame& data);
 
-	/* --------------------------------------------------------------------
-	 * Protected
-	 * --------------------------------------------------------------------
-	 */
+            /**
+             * Query value from output port "image"
+             * @return image: same image, filtered
+             */
+            virtual const asn1SccFrame& imageOutput() const;
+
         protected:
-            FrameWrapper::FrameConstPtr inImage;
-            FrameWrapper::FrameConstPtr outFilteredImage;
 
-	/* --------------------------------------------------------------------
-	 * Private
-	 * --------------------------------------------------------------------
-	 */
-	private:
+            asn1SccFrame inImage;
+            asn1SccFrame outImage;
     };
 }
-#endif
-/* ImageFilteringInterface.hpp */
+
+#endif // IMAGEFILTERING_INTERFACE_HPP
+
 /** @} */
