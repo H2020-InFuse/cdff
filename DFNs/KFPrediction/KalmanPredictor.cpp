fb903e5cf1cbe1437d25ce7d6664ff5d0d4e93cf
Romain Michalec <romain.michalec@strath.ac.uk>
2018-05-28 13:40:37 +0100

    Port Kalman predictor to new interface

--- DFNs/KFPrediction/KalmanPredictor.cpp
+++ DFNs/KFPrediction/KalmanPredictor.cpp
@@ -1,221 +1,195 @@
-/* --------------------------------------------------------------------------
-*
-* (C) Copyright …
-*
-* ---------------------------------------------------------------------------
-*/
-
-/*!
- * @file KalmanPredictor.cpp
- * @date 26/04/2018
+/**
  * @author Nassir W. Oumer
  */
 
-/*!
+/**
  * @addtogroup DFNs
- * 
- * KalmanPredictor Implementation .
- * 
- * 
  * @{
  */
 
-/* --------------------------------------------------------------------------
- *
- * Includes
- *
- * --------------------------------------------------------------------------
- */
-
-
 #include "KalmanPredictor.hpp"
-#include <Errors/Assert.hpp>
+
 #include <Macros/YamlcppMacros.hpp>
+#include <Errors/Assert.hpp>
 
 #include <stdlib.h>
 #include <fstream>
 
 using namespace Helpers;
 
-namespace dfn_ci {
+namespace dfn_ci
+{
 
-KalmanPredictor::KalmanPredictor():timeOfLastValidCorrection(-1),KF(12,6,0),Tpred(4,4,CV_32F),Test(4,4,CV_32F)
-	{
-	
+KalmanPredictor::KalmanPredictor()
+	: timeOfLastValidCorrection(-1),KF(12,6,0),Tpred(4,4,CV_32F),Test(4,4,CV_32F)
+{
 	parametersHelper.AddParameter<float>("KalmanParameters", "StandardDeviationtdOrientation", parameters.kalmanParameters.stdOrientation, DEFAULT_PARAMETERS.kalmanParameters.stdOrientation);
 	parametersHelper.AddParameter<float>("KalmanParameters", "StandardDeviationtdTranslation", parameters.kalmanParameters.stdTranslation, DEFAULT_PARAMETERS.kalmanParameters.stdOrientation);
 	parametersHelper.AddParameter<float>("KalmanParameters", "InitialStateCovariance", parameters.kalmanParameters.initialCovariance, DEFAULT_PARAMETERS.kalmanParameters.initialCovariance);
-		
+
 	configurationFilePath = "";
-	}
+}
 
 KalmanPredictor::~KalmanPredictor()
 {
-
 }
 
-void KalmanPredictor::configure() 
+void KalmanPredictor::configure()
 {
-    parametersHelper.ReadFile(configurationFilePath);
-    ValidateParameters();
+	parametersHelper.ReadFile(configurationFilePath);
+	ValidateParameters();
 }
 
+void KalmanPredictor::process()
+{
 
-void KalmanPredictor::process() {
-		
-   	cv::Mat pose(6,1,CV_32F);
+	cv::Mat pose(6,1,CV_32F);
 	cv::Mat vel0(6,1,CV_32F);
-	float time0=0; //pose initialization time stamp
-	float currentTime; //image timestamp
-			
-	for(int i=0;i<3;i++)
-	{		
-       		pose.at<float>(i,0)=inpreviousstate.orient.arr[i];
-		pose.at<float>(i+3,0)=inpreviousstate.pos.arr[i];
-		vel0.at<float>(i,0)=inpreviousstate.angular_velocity.arr[i];
-		vel0.at<float>(i+3,0)=inpreviousstate.velocity.arr[i];
+	float time0 = 0;   // pose initialization timestamp
+	float currentTime; // image timestamp
+
+	// Read data from input port
+	for (int i = 0; i < 3; i++)
+	{
+		pose.at<float>(i,0) = inPreviousState.orient.arr[i];
+		pose.at<float>(i+3,0) = inPreviousState.pos.arr[i];
+		vel0.at<float>(i,0) = inPreviousState.angular_velocity.arr[i];
+		vel0.at<float>(i+3,0) = inPreviousState.velocity.arr[i];
 	}
 
-	currentTime= (float)inpreviousstate.timestamp.microseconds;
-	
+	currentTime = (float)inPreviousState.timestamp.microseconds;
+
+	// Process data
 	ValidateInputs(pose);
-	
-	//DFN user should decide when to initialize
-	if(time0==0)
-	 init(pose,vel0,time0);
-	
-	
-	cv::Mat predictor=predict(currentTime);
-	
-	for(int i=0;i<3;i++)
+
+	if (time0 == 0) // DFN user should decide when to initialize
 	{
-	 	outpredictedstate.orient.arr[i]=predictor.at<float>(i);
-		outpredictedstate.pos.arr[i]=predictor.at<float>(i+3);
-		outpredictedstate.angular_velocity.arr[i]=predictor.at<float>(i+6);
-		outpredictedstate.velocity.arr[i]=predictor.at<float>(i+9);
+		init(pose,vel0,time0);
 	}
 
-	cv::Mat covPredictor=predict();
-	
-	for(int row=0;row<3;row++)
+	cv::Mat predictor = predict(currentTime);
+
+	// Write data to output port
+	for (int i = 0; i < 3; i++)
 	{
-		for(int col=0;col<3;col++)
+		outPredictedState.orient.arr[i] = predictor.at<float>(i);
+		outPredictedState.pos.arr[i] = predictor.at<float>(i+3);
+		outPredictedState.angular_velocity.arr[i] = predictor.at<float>(i+6);
+		outPredictedState.velocity.arr[i] = predictor.at<float>(i+9);
+	}
+
+	cv::Mat covPredictor = predict();
+
+	for (int row = 0; row < 3; row++)
+	{
+		for (int col = 0; col < 3; col++)
 		{
-	 		outpredictedStateCovariance.cov_orientation.arr[row].arr[col]=covPredictor.at<float>(row,col);
-			outpredictedStateCovariance.cov_position.arr[row].arr[col]=covPredictor.at<float>(row+3,col+3);
-			outpredictedStateCovariance.cov_angular_velocity.arr[row].arr[col]=covPredictor.at<float>(row+6,col+6);
-			outpredictedStateCovariance.cov_velocity.arr[row].arr[col]=covPredictor.at<float>(row+9,col+9);
-		
+			outPredictedStateCovariance.cov_orientation.arr[row].arr[col] = covPredictor.at<float>(row,col);
+			outPredictedStateCovariance.cov_position.arr[row].arr[col] = covPredictor.at<float>(row+3,col+3);
+			outPredictedStateCovariance.cov_angular_velocity.arr[row].arr[col] = covPredictor.at<float>(row+6,col+6);
+			outPredictedStateCovariance.cov_velocity.arr[row].arr[col] = covPredictor.at<float>(row+9,col+9);
 		}
 	}
-
-		
 }
 
 const KalmanPredictor::KalmanPredictorOptionsSet KalmanPredictor::DEFAULT_PARAMETERS =
-	{
+{
 	.kalmanParameters =
-		{
+	{
 		.stdOrientation =1000.0,
 		.stdTranslation=1000.0,
 		.initialCovariance= 10000
-		
-		}
-	
-	};
+	}
+};
 
 cv::Mat KalmanPredictor::predict(float currentTime)
-	{
+{
 	cv::Mat statePredict;
 	float stdOrientation, stdTranslation;
-        stdOrientation = parameters.kalmanParameters.stdOrientation;
+	stdOrientation = parameters.kalmanParameters.stdOrientation;
 	stdTranslation = parameters.kalmanParameters.stdTranslation;
 	float dt = currentTime-timeOfLastValidCorrection;
-	
-        cv::Mat G_rot_temp1(3,6,CV_32F,cv::Scalar(0));
+
+	cv::Mat G_rot_temp1(3,6,CV_32F,cv::Scalar(0));
 	cv::Mat G_rot_temp2(3,6,CV_32F,cv::Scalar(0));
-	 
+
 	cv::Mat G_trans_temp1(3,6,CV_32F,cv::Scalar(0));
 	cv::Mat G_trans_temp2(3,6,CV_32F,cv::Scalar(0));
-	
-	for(int i=0;i<3;i++)
-	    {
-		  for(int j=0;j<3;j++)
-		 {
+
+	for (int i=0;i<3;i++)
+	{
+		for (int j=0;j<3;j++)
+		{
 			G_rot_temp1.at<float>(i,i)=0.5*dt*dt*stdOrientation;
 			G_rot_temp2.at<float>(i,i+3)=dt*stdOrientation;
 
 			G_trans_temp1.at<float>(i,i)=0.5*dt*dt*stdTranslation;
 			G_trans_temp2.at<float>(i,i+3)=dt*stdTranslation;
 		}
-	     }
-		
-	 cv::Mat G1(6,6,CV_32F);
-	 cv::vconcat(G_rot_temp1,G_rot_temp2, G1);
+	}
 
-	 cv::Mat G2(6,6,CV_32F);
-	 cv::vconcat(G_trans_temp1,G_trans_temp2, G2);
+	cv::Mat G1(6,6,CV_32F);
+	cv::vconcat(G_rot_temp1,G_rot_temp2, G1);
 
-	 cv::Mat G(12,6,CV_32F);
-	 cv::vconcat(G1,G2, G);
-	 	
-	 cv::Mat Gt(6,12,CV_32F);
-	 Gt= G.t();
-			
-	 KF.processNoiseCov =G*Gt;       
-		
-	setIdentity(KF.transitionMatrix, cv::Scalar::all(1)); 
-	KF.transitionMatrix.at<float>(0,6)=dt; 
-	KF.transitionMatrix.at<float>(1,7)=dt; 
-	KF.transitionMatrix.at<float>(2,8)=dt; 
-	KF.transitionMatrix.at<float>(3,9)=dt; 
-	KF.transitionMatrix.at<float>(4,10)=dt; 
-	KF.transitionMatrix.at<float>(5,11)=dt; 
-	
+	cv::Mat G2(6,6,CV_32F);
+	cv::vconcat(G_trans_temp1,G_trans_temp2, G2);
+
+	cv::Mat G(12,6,CV_32F);
+	cv::vconcat(G1,G2, G);
+
+	cv::Mat Gt(6,12,CV_32F);
+	Gt= G.t();
+
+	KF.processNoiseCov =G*Gt;
+
+	setIdentity(KF.transitionMatrix, cv::Scalar::all(1));
+	KF.transitionMatrix.at<float>(0,6)=dt;
+	KF.transitionMatrix.at<float>(1,7)=dt;
+	KF.transitionMatrix.at<float>(2,8)=dt;
+	KF.transitionMatrix.at<float>(3,9)=dt;
+	KF.transitionMatrix.at<float>(4,10)=dt;
+	KF.transitionMatrix.at<float>(5,11)=dt;
+
 	statePredict=KF.predict();
 
-		
 	return statePredict;
+}
 
-	}
 cv::Mat KalmanPredictor::predict()
-	{
+{
 	cv::Mat A=KF.transitionMatrix;
 	cv::Mat Q=KF.processNoiseCov;
 	KF.errorCovPre=A*KF.errorCovPost*A+Q;
-	
-	return KF.errorCovPre;
-	}
 
-void KalmanPredictor::init(cv::Mat &pose, cv::Mat &vel0, float time0) 
-	{
-	        		
+	return KF.errorCovPre;
+}
+
+void KalmanPredictor::init(cv::Mat &pose, cv::Mat &vel0, float time0)
+{
+
 	for (int i=0;i<6;i++)
-	{	
+	{
 		KF.statePost.at<float>(i)= 0;
 		KF.statePost.at<float>(i+6)=vel0.at<float>(i,0);
 	}
-	 
+
 	float initialCovariance;
 	initialCovariance = parameters.kalmanParameters.initialCovariance;
 	setIdentity(KF.errorCovPost, cv::Scalar::all(initialCovariance));
 
 	timeOfLastValidCorrection = time0;
-
-	}
+}
 
 void KalmanPredictor::ValidateParameters()
-	{
+{
 	ASSERT(parameters.kalmanParameters.initialCovariance > 100, " Kalman Predictor Configuration error: initial state covariance too small");
-	
-	}
+}
 
 void KalmanPredictor::ValidateInputs(cv::Mat pose)
-	{
-	
+{
 	ASSERT(pose.rows == 6 && pose.cols ==1, "Kalman Predictor: Predictor input is 6-d column vector");
-	}
-	
-
+}
 
 }
+
+/** @} */
--- DFNs/KFPrediction/KalmanPredictor.hpp
+++ DFNs/KFPrediction/KalmanPredictor.hpp
@@ -1,101 +1,83 @@
-/* --------------------------------------------------------------------------
-*
-* (C) Copyright …
-*
-* --------------------------------------------------------------------------
-*/
-
-/*!
- * @file KalmanPredictor.hpp
- * @date 24/04/2018
+/**
  * @author Nassir W. Oumer
  */
 
-/*!
+/**
  * @addtogroup DFNs
- * 
- *  @brief This DFN executes a Kalman Predictor.
- *  
- * This DFN is Kalman Predictor implementation of OpenCV.
- * 
- * states are incremental local motions dp and associated derivatives vel
- * Constant velocity model, between two camera frames  
- * For pose estimation, the user of this DFN can use predicted states, for example convert to absolute predicted pose as T_k+1=T_k*dT(dp)
- * This DFN implementation requires the following parameters:
- * @param  porcess noise parameters, standard deviation of the random acc (linear and angualr)  .
- * @param, initial covariance, roto-translation and velocity components .
- *
  * @{
  */
 
 #ifndef KALMANPREDICTOR_HPP
 #define KALMANPREDICTOR_HPP
-/* --------------------------------------------------------------------------
- *
- * Includes
- *
- * --------------------------------------------------------------------------
- */
-#include <KFPrediction/KFPredictionInterface.hpp>
-#include "opencv2/video/tracking.hpp"
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/imgcodecs/imgcodecs.hpp>
-#include <yaml-cpp/yaml.h>
+
+#include "KFPredictionInterface.hpp"
+
 #include <Helpers/ParametersListHelper.hpp>
 
+#include <yaml-cpp/yaml.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/video/tracking.hpp>
 
+namespace dfn_ci
+{
+	/**
+	 * Kalman predictor DFN (algorithm provided by OpenCV)
+	 *
+	 * States are local motion increments dp and the associated derivatives vel.
+	 * Constant velocity model between two camera frames. For pose estimation,
+	 * the user of this DFN can use predicted states, for example convert to
+	 * absolute predicted pose as T_{k+1} = T_k * dT(dp)
+	 *
+	 * @param  process noise parameters
+	 *         standard deviation of the random (linear and angular) acceleration
+	 * @param initial covariance
+	 *        roto-translation and velocity components
+	 */
+	class KalmanPredictor : public KFPredictionInterface
+	{
+		public:
 
-namespace dfn_ci {
+			KalmanPredictor();
+			virtual ~KalmanPredictor();
 
-/* --------------------------------------------------------------------------
- *
- * Class definition
- *
- * --------------------------------------------------------------------------
- */
-    class KalmanPredictor : public KFPredictionInterface
-    {
-        public:
-            KalmanPredictor();
-            virtual ~KalmanPredictor();
-            virtual void process();
-            virtual void configure();
-	   
-	    float timeOfLastValidCorrection;
-	    cv::KalmanFilter KF; 
-	    void init(cv::Mat &pose,cv::Mat &vel0,float time0);
-	    cv::Mat Tpred;
-            cv::Mat Test;
+			virtual void configure();
+			virtual void process();
 
-	private:
-	
-		struct KalmanParameters
+			float timeOfLastValidCorrection;
+			cv::KalmanFilter KF;
+			void init(cv::Mat &pose, cv::Mat &vel0, float time0);
+			cv::Mat Tpred;
+			cv::Mat Test;
+
+		private:
+
+			struct KalmanParameters
 			{
 				float stdOrientation;
 				float stdTranslation;
 				float initialCovariance;
-				
 			};
-		
-		struct KalmanPredictorOptionsSet
+
+			struct KalmanPredictorOptionsSet
 			{
 				KalmanParameters kalmanParameters;
 			};
-		
-		Helpers::ParametersListHelper parametersHelper;
-		KalmanPredictorOptionsSet parameters;
-		
-		static const KalmanPredictorOptionsSet DEFAULT_PARAMETERS;
-		
-		cv::Mat predict(float currentTime);
-		cv::Mat predict();
-	
-		void ValidateParameters();
-		void ValidateInputs(cv::Mat inputState);
 
-		void Configure(const YAML::Node& configurationNode);
+			Helpers::ParametersListHelper parametersHelper;
+			KalmanPredictorOptionsSet parameters;
 
+			static const KalmanPredictorOptionsSet DEFAULT_PARAMETERS;
+
+			cv::Mat predict(float currentTime);
+			cv::Mat predict();
+
+			void ValidateParameters();
+			void ValidateInputs(cv::Mat inputState);
+
+			void Configure(const YAML::Node& configurationNode);
 	};
 }
-#endif
+
+#endif // KALMANPREDICTOR_HPP
+
+/** @} */
