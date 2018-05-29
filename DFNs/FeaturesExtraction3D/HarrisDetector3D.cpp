9948c1d4fe41f4600ee298cb2bdc73244a0325bb
Romain Michalec <romain.michalec@strath.ac.uk>
2018-05-29 02:34:13 +0100

    Port 3D Harris detector to new interface

--- DFNs/FeaturesExtraction3D/HarrisDetector3D.cpp
+++ DFNs/FeaturesExtraction3D/HarrisDetector3D.cpp
@@ -1,58 +1,31 @@
-/* --------------------------------------------------------------------------
-*
-* (C) Copyright …
-*
-* ---------------------------------------------------------------------------
-*/
-
-/*!
- * @file HarrisDetector3D.cpp
- * @date 01/12/2017
+/**
  * @author Alessandro Bianco
  */
 
-/*!
+/**
  * @addtogroup DFNs
- * 
- * Implementation of the Harris Detector 3D class.
- * 
- * 
  * @{
  */
 
-/* --------------------------------------------------------------------------
- *
- * Includes
- *
- * --------------------------------------------------------------------------
- */
 #include "HarrisDetector3D.hpp"
-#include <Errors/Assert.hpp>
+
 #include <PointCloudToPclPointCloudConverter.hpp>
 #include <MatToVisualPointFeatureVector3DConverter.hpp>
-#include <ConversionCache/ConversionCache.hpp>
 #include <Macros/YamlcppMacros.hpp>
+#include <Errors/Assert.hpp>
 
 #include <stdlib.h>
 #include <fstream>
 
-
-using namespace Common;
 using namespace Converters;
 using namespace VisualPointFeatureVector3DWrapper;
 using namespace PointCloudWrapper;
 
-namespace dfn_ci {
+namespace dfn_ci
+{
 
-
-/* --------------------------------------------------------------------------
- *
- * Public Member Functions
- *
- * --------------------------------------------------------------------------
- */
 HarrisDetector3D::HarrisDetector3D()
-	{
+{
 	parametersHelper.AddParameter<bool>("GeneralParameters", "NonMaxSuppression", parameters.nonMaxSuppression, DEFAULT_PARAMETERS.nonMaxSuppression);
 	parametersHelper.AddParameter<float>("GeneralParameters", "Radius", parameters.radius, DEFAULT_PARAMETERS.radius);
 	parametersHelper.AddParameter<float>("GeneralParameters", "SearchRadius", parameters.searchRadius, DEFAULT_PARAMETERS.searchRadius);
@@ -63,89 +36,90 @@ HarrisDetector3D::HarrisDetector3D()
 	parametersHelper.AddParameter<OutputFormat, OutputFormatHelper>("GeneralParameters", "OutputFormat", parameters.outputFormat, DEFAULT_PARAMETERS.outputFormat);
 
 	configurationFilePath = "";
-	}
+}
 
 HarrisDetector3D::~HarrisDetector3D()
-	{
-
-	}
+{
+}
 
 void HarrisDetector3D::configure()
-	{
+{
 	parametersHelper.ReadFile(configurationFilePath);
 	ValidateParameters();
-	}
+}
 
-
-void HarrisDetector3D::process() 
+void HarrisDetector3D::process()
+{
+	// Read data from input port
+	if (GetNumberOfPoints(inPointcloud) == 0)
 	{
-	if (GetNumberOfPoints(*inPointCloud) == 0)
-		{
-		outFeaturesSet = NewVisualPointFeatureVector3D();
 		return;
-		}
+	}
+	pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputPointCloud =
+		pointCloudToPclPointCloudConverter.Convert(&inPointcloud);
 
-	pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputPointCloud = 
-		ConversionCache<PointCloudConstPtr, pcl::PointCloud<pcl::PointXYZ>::ConstPtr, PointCloudToPclPointCloudConverter>::Convert(inPointCloud);
+	// Process data
 	ValidateInputs(inputPointCloud);
 	pcl::PointIndicesConstPtr harrisPoints = ComputeHarrisPoints(inputPointCloud);
-	outFeaturesSet = Convert(inputPointCloud, harrisPoints);
-	}
 
-HarrisDetector3D::OutputFormatHelper::OutputFormatHelper(const std::string& parameterName, OutputFormat& boundVariable, const OutputFormat& defaultValue) :
-	ParameterHelper(parameterName, boundVariable, defaultValue)
-	{
+	// Write data to output port
+	VisualPointFeatureVector3DConstPtr tmp = Convert(inputPointCloud, harrisPoints);
+	Copy(*tmp, outFeatures);
+	delete(tmp);
+}
 
-	}
+HarrisDetector3D::OutputFormatHelper::OutputFormatHelper(const std::string& parameterName, OutputFormat& boundVariable, const OutputFormat& defaultValue)
+	: ParameterHelper(parameterName, boundVariable, defaultValue)
+{
+}
 
 HarrisDetector3D::OutputFormat HarrisDetector3D::OutputFormatHelper::Convert(const std::string& outputFormat)
-	{
+{
 	if (outputFormat == "Positions" || outputFormat == "0")
-		{
+	{
 		return POSITIONS_OUTPUT;
-		}
+	}
 	else if (outputFormat == "References" || outputFormat == "1")
-		{
+	{
 		return REFERENCES_OUTPUT;
-		}
+	}
 	ASSERT(false, "ShotDescriptor3d Error: unhandled output format");
 	return POSITIONS_OUTPUT;
-	}
+}
 
-HarrisDetector3D::HarrisMethodHelper::HarrisMethodHelper(const std::string& parameterName, HarrisMethod& boundVariable, const HarrisMethod& defaultValue) :
-	ParameterHelper(parameterName, boundVariable, defaultValue)
-	{
-
-	}
+HarrisDetector3D::HarrisMethodHelper::HarrisMethodHelper(const std::string& parameterName, HarrisMethod& boundVariable, const HarrisMethod& defaultValue)
+	: ParameterHelper(parameterName, boundVariable, defaultValue)
+{
+}
 
 HarrisDetector3D::HarrisMethod HarrisDetector3D::HarrisMethodHelper::Convert(const std::string& method)
-	{
+{
 	if (method == "Harris" || method == "0")
-		{
+	{
 		return pcl::HarrisKeypoint3D<pcl::PointXYZ, pcl::PointXYZI>::HARRIS;
-		}
+	}
 	else if (method == "Noble" || method == "1")
-		{
+	{
 		return pcl::HarrisKeypoint3D<pcl::PointXYZ, pcl::PointXYZI>::NOBLE;
-		}
+	}
 	else if (method == "Lowe" || method == "2")
-		{
+	{
 		return pcl::HarrisKeypoint3D<pcl::PointXYZ, pcl::PointXYZI>::LOWE;
-		}
+	}
 	else if (method == "Tomasi" || method == "3")
-		{
+	{
 		return pcl::HarrisKeypoint3D<pcl::PointXYZ, pcl::PointXYZI>::TOMASI;
-		}
+	}
 	else if (method == "Curvature" || method == "4")
-		{
+	{
 		return pcl::HarrisKeypoint3D<pcl::PointXYZ, pcl::PointXYZI>::CURVATURE;
-		}
-
-	ASSERT(false, "HarrisDetector3D Configuration error, Harris Method is not one of {Harris, Noble, Lowe, Tomasi, Curvature}");
 	}
 
+	ASSERT(false, "HarrisDetector3D Configuration error, Harris Method is not one of {Harris, Noble, Lowe, Tomasi, Curvature}");
+}
+
 const HarrisDetector3D::HarryOptionsSet HarrisDetector3D::DEFAULT_PARAMETERS
-	{
+{
 	.nonMaxSuppression = true,
 	.radius = 0.01,
 	.searchRadius = 0.01,
@@ -154,71 +128,67 @@ const HarrisDetector3D::HarryOptionsSet HarrisDetector3D::DEFAULT_PARAMETERS
 	.numberOfThreads = 0,
 	.method = pcl::HarrisKeypoint3D<pcl::PointXYZ, pcl::PointXYZI>::HARRIS,
 	.outputFormat = POSITIONS_OUTPUT,
-	};
-
+};
 
 pcl::PointIndicesConstPtr HarrisDetector3D::ComputeHarrisPoints(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud)
-	{
-	pcl::HarrisKeypoint3D<pcl::PointXYZ, pcl::PointXYZI> detector; 
-    	detector.setNonMaxSupression (parameters.nonMaxSuppression);
-    	detector.setRadius (parameters.radius);
-    	detector.setRadiusSearch (parameters.searchRadius);
+{
+	pcl::HarrisKeypoint3D<pcl::PointXYZ, pcl::PointXYZI> detector;
+	detector.setNonMaxSupression (parameters.nonMaxSuppression);
+	detector.setRadius (parameters.radius);
+	detector.setRadiusSearch (parameters.searchRadius);
 	detector.setMethod (parameters.method);
 	detector.setThreshold(parameters.detectionThreshold);
 	detector.setRefine(parameters.enableRefinement);
 	detector.setNumberOfThreads(parameters.numberOfThreads);
-	//ISSUE: should we use this one more parameter? 
+	//ISSUE: should we use this one more parameter?
 	//detector.setSearchSurface(pointCloud);
-    	detector.setInputCloud(pointCloud);
+	detector.setInputCloud(pointCloud);
 
-    	pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints = boost::make_shared<pcl::PointCloud<pcl::PointXYZI> >();
-    	detector.compute(*keypoints); 
+	pcl::PointCloud<pcl::PointXYZI>::Ptr keypoints = boost::make_shared<pcl::PointCloud<pcl::PointXYZI> >();
+	detector.compute(*keypoints);
 	pcl::PointIndicesConstPtr harrisPoints = detector.getKeypointsIndices();
 
 	return harrisPoints;
-	}
+}
 
 VisualPointFeatureVector3DConstPtr HarrisDetector3D::Convert(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputCloud, const pcl::PointIndicesConstPtr indicesList)
-	{
+{
 	VisualPointFeatureVector3DPtr featuresVector = new VisualPointFeatureVector3D();
 	ClearPoints(*featuresVector);
-	for(unsigned pointIndex = 0; pointIndex < indicesList->indices.size(); pointIndex++)
-		{
+	for (unsigned pointIndex = 0; pointIndex < indicesList->indices.size(); pointIndex++)
+	{
 		if (parameters.outputFormat == POSITIONS_OUTPUT)
-			{
+		{
 			pcl::PointXYZ point = inputCloud->points.at(indicesList->indices.at(pointIndex));
 			AddPoint(*featuresVector, point.x, point.y, point.z);
-			}
-		else if (parameters.outputFormat == REFERENCES_OUTPUT)
-			{
-			AddPoint(*featuresVector, indicesList->indices.at(pointIndex) );
-			}
 		}
-	return featuresVector;
+		else if (parameters.outputFormat == REFERENCES_OUTPUT)
+		{
+			AddPoint(*featuresVector, indicesList->indices.at(pointIndex) );
+		}
 	}
-
+	return featuresVector;
+}
 
 void HarrisDetector3D::ValidateParameters()
-	{
+{
 	ASSERT( parameters.radius > 0, "HarrisDetector3D Configuration error, radius is not positive");
 	ASSERT( parameters.numberOfThreads >= 0, "HarrisDetector3D Configuration error, number of threads is negative");
 	VERIFY( parameters.nonMaxSuppression || parameters.detectionThreshold < std::numeric_limits<float>::epsilon(), "Warning HarrisDetector3D: ineffective threshold when non max suppression is false");
-	}
-
-void HarrisDetector3D::ValidateInputs(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud)
-	{
-	for(unsigned pointIndex; pointIndex < pointCloud->points.size(); pointIndex++)
-		{
-		pcl::PointXYZ point = pointCloud->points.at(pointIndex);
-		if (point.x != point.x || point.y != point.y || point.z != point.z)
-			{
-			ASSERT(false, "HarrisDetector3D Error: Invalid point in input point cloud");
-			}
-		}
-	}
-
-
 }
 
+void HarrisDetector3D::ValidateInputs(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud)
+{
+	for (unsigned pointIndex; pointIndex < pointCloud->points.size(); pointIndex++)
+	{
+		pcl::PointXYZ point = pointCloud->points.at(pointIndex);
+		if (point.x != point.x || point.y != point.y || point.z != point.z)
+		{
+			ASSERT(false, "HarrisDetector3D Error: Invalid point in input point cloud");
+		}
+	}
+}
+
+}
 
 /** @} */
--- DFNs/FeaturesExtraction3D/HarrisDetector3D.hpp
+++ DFNs/FeaturesExtraction3D/HarrisDetector3D.hpp
@@ -1,142 +1,111 @@
-/* --------------------------------------------------------------------------
-*
-* (C) Copyright …
-*
-* --------------------------------------------------------------------------
-*/
-
-/*!
- * @file HarrisDetector3D.hpp
- * @date 01/12/2017
+/**
  * @author Alessandro Bianco
  */
 
-/*!
+/**
  * @addtogroup DFNs
- * 
- *  This DFN implements the Harris Detector for 3D Point Clouds.
- *  
- *
  * @{
  */
 
-/*!
- * @addtogroup DFNs
- * 
- *  @brief This DFN executes the Harris Detector algorithm for extraction of 3d keypoints from a point cloud. 
- * 
- * This DFN implementation requires the following parameters:
- * @param nonMaxSuppression, 
- * @param radius, 
- * @param searchRadius,
- * @param detectionThreshold,
- * @param enableRefinement,
- * @param numberOfThreads,
- * @param method,
- * @param outputFormat, this defines the output format of the keypoints cloud. If "Positions" is chosen, then the output point cloud contains the 3d coordinates of the keypoints, if "References" is chosen
- * 			then the output point cloud contains the indices of the 3d point as they appear in the input point cloud.
- *
- * @{
- */
+#ifndef HARRISDETECTOR3D_HPP
+#define HARRISDETECTOR3D_HPP
 
-#ifndef HARRIS_DETECTOR_3D_HPP
-#define HARRIS_DETECTOR_3D_HPP
+#include "FeaturesExtraction3DInterface.hpp"
 
-/* --------------------------------------------------------------------------
- *
- * Includes
- *
- * --------------------------------------------------------------------------
- */
-#include <FeaturesExtraction3D/FeaturesExtraction3DInterface.hpp>
 #include <PointCloud.hpp>
 #include <VisualPointFeatureVector3D.hpp>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <stdlib.h>
-#include <string>
-#include <pcl/keypoints/harris_3d.h>
-#include <yaml-cpp/yaml.h>
+#include <PointCloudToPclPointCloudConverter.hpp>
 #include <Helpers/ParametersListHelper.hpp>
 
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/keypoints/harris_3d.h>
+#include <yaml-cpp/yaml.h>
 
-namespace dfn_ci {
+#include <stdlib.h>
+#include <string>
 
-/* --------------------------------------------------------------------------
- *
- * Class definition
- *
- * --------------------------------------------------------------------------
- */
-    class HarrisDetector3D : public FeaturesExtraction3DInterface
-    {
-	/* --------------------------------------------------------------------
-	 * Public
-	 * --------------------------------------------------------------------
+namespace dfn_ci
+{
+	/**
+	 * Extraction of keypoints in a 3D pointcloud using the Harris Detector
+	 * algorithm.
+	 *
+	 * @param nonMaxSuppression
+	 * @param radius
+	 * @param searchRadius
+	 * @param detectionThreshold
+	 * @param enableRefinement
+	 * @param numberOfThreads
+	 * @param method
+	 * @param outputFormat
+	 *        Format of the returned pointcloud of keypoints:
+	 *        * Positions:
+	 *          The pointcloud contains the 3D coordinates of the keypoints
+	 *        * References:
+	 *          The pointcloud contains the indices of the 3D points,
+	 *          as they appear in the input pointcloud
 	 */
-        public:
-            HarrisDetector3D();
-            ~HarrisDetector3D();
-            void process();
-            void configure();
+	class HarrisDetector3D : public FeaturesExtraction3DInterface
+	{
+		public:
 
-	/* --------------------------------------------------------------------
-	 * Protected
-	 * --------------------------------------------------------------------
-	 */
-        protected:
+			HarrisDetector3D();
+			virtual ~HarrisDetector3D();
 
-	/* --------------------------------------------------------------------
-	 * Private
-	 * --------------------------------------------------------------------
-	 */	
-	private:
-		
-		enum OutputFormat
+			virtual void configure();
+			virtual void process();
+
+		private:
+
+			enum OutputFormat
 			{
-			POSITIONS_OUTPUT,
-			REFERENCES_OUTPUT
+				POSITIONS_OUTPUT,
+				REFERENCES_OUTPUT
 			};
-		class OutputFormatHelper : public Helpers::ParameterHelper<OutputFormat, std::string>
+			class OutputFormatHelper : public Helpers::ParameterHelper<OutputFormat, std::string>
 			{
-			public:
-				OutputFormatHelper(const std::string& parameterName, OutputFormat& boundVariable, const OutputFormat& defaultValue);
-			private:
-				OutputFormat Convert(const std::string& value);
+				public:
+					OutputFormatHelper(const std::string& parameterName, OutputFormat& boundVariable, const OutputFormat& defaultValue);
+				private:
+					OutputFormat Convert(const std::string& value);
 			};
 
-		typedef pcl::HarrisKeypoint3D<pcl::PointXYZ, pcl::PointXYZI>::ResponseMethod HarrisMethod;
-		class HarrisMethodHelper : public Helpers::ParameterHelper<HarrisMethod, std::string>
+			typedef pcl::HarrisKeypoint3D<pcl::PointXYZ, pcl::PointXYZI>::ResponseMethod HarrisMethod;
+			class HarrisMethodHelper : public Helpers::ParameterHelper<HarrisMethod, std::string>
 			{
-			public:
-				HarrisMethodHelper(const std::string& parameterName, HarrisMethod& boundVariable, const HarrisMethod& defaultValue);
-			private:
-				HarrisMethod Convert(const std::string& value);
+				public:
+					HarrisMethodHelper(const std::string& parameterName, HarrisMethod& boundVariable, const HarrisMethod& defaultValue);
+				private:
+					HarrisMethod Convert(const std::string& value);
 			};
 
-		struct HarryOptionsSet
+			struct HarryOptionsSet
 			{
-			bool nonMaxSuppression;
-			float radius;
-			float searchRadius;
-			float detectionThreshold;
-			bool enableRefinement;
-			int numberOfThreads;
-			HarrisMethod method;
-			OutputFormat outputFormat;
+				bool nonMaxSuppression;
+				float radius;
+				float searchRadius;
+				float detectionThreshold;
+				bool enableRefinement;
+				int numberOfThreads;
+				HarrisMethod method;
+				OutputFormat outputFormat;
 			};
 
-		Helpers::ParametersListHelper parametersHelper;
-		HarryOptionsSet parameters;
-		static const HarryOptionsSet DEFAULT_PARAMETERS;
+			Helpers::ParametersListHelper parametersHelper;
+			HarryOptionsSet parameters;
+			static const HarryOptionsSet DEFAULT_PARAMETERS;
 
-		pcl::PointIndicesConstPtr ComputeHarrisPoints(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud);
-		VisualPointFeatureVector3DWrapper::VisualPointFeatureVector3DConstPtr Convert(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputCloud, const pcl::PointIndicesConstPtr indicesList);
+			Converters::PointCloudToPclPointCloudConverter pointCloudToPclPointCloudConverter;
 
-		void ValidateParameters();
-		void ValidateInputs(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud);
-    };
+			pcl::PointIndicesConstPtr ComputeHarrisPoints(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud);
+			VisualPointFeatureVector3DWrapper::VisualPointFeatureVector3DConstPtr Convert(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr inputCloud, const pcl::PointIndicesConstPtr indicesList);
+
+			void ValidateParameters();
+			void ValidateInputs(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud);
+	};
 }
-#endif
-/* HarrisDetector3D.hpp */
+
+#endif // HARRISDETECTOR3D_HPP
+
 /** @} */
