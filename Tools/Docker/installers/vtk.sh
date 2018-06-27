23d327dc4dc3237d9e4a0b24046e1800424b55b9
Thibaud Chupin <thibaud.chupin@gmail.com>
2018-06-27 15:34:10 +0200

    Unify formatting of install scripts

--- Tools/Docker/installers/vtk.sh
+++ Tools/Docker/installers/vtk.sh
@@ -5,10 +5,17 @@
 
 function install4infuse_vtk {
 if [[ ! -n $(find $PKG_DIR -name 'vtk*') ]]; then
-	fetchgit_function vtk v8.1.0 https://github.com/Kitware/vtk.git
-	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR -D BUILD_TESTING=OFF -D BUILD_EXAMPLES=OFF -D BUILD_DOCUMENTATION=OFF $SOURCE_DIR/vtk
-	make --jobs=${CPUS}
-	install_function vtk 8.1.0
-	clean_function vtk
+    fetchgit_function vtk v8.1.0 https://github.com/Kitware/vtk.git
+    cmake \
+        -D CMAKE_BUILD_TYPE=RELEASE \
+        -D CMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -D BUILD_TESTING=OFF \
+        -D BUILD_EXAMPLES=OFF \
+        -D BUILD_DOCUMENTATION=OFF \
+        $SOURCE_DIR/vtk
+
+    make --jobs=${CPUS}
+    install_function vtk 8.1.0
+    clean_function vtk
 fi
 }
