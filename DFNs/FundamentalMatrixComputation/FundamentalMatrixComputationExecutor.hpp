b32a21723b8b143c1f2726cdf5d653811ef011b9
Alessandro Bianco <alessandro.bianco@strath.ac.uk>
2018-08-20 15:54:33 +0100

    Adding more Executor documentation

--- DFNs/FundamentalMatrixComputation/FundamentalMatrixComputationExecutor.hpp
+++ DFNs/FundamentalMatrixComputation/FundamentalMatrixComputationExecutor.hpp
@@ -24,6 +24,7 @@ namespace DFN
 * The main difference between the four methods are input and output types:
 * Methods (i) and (ii) have the constant pointer as input, Methods (iii)  and (iv) have a constant reference as input;
 * Methods (i) and (iii) are non-creation methods, they give constant pointers as output, the output is just the output reference in the DFN;
+* When using creation methods, the output has to be initialized to NULL.
 * Methods (ii) and (iv) are creation methods, they copy the output of the DFN in the referenced output variable. Method (ii) takes a pointer, method (iv) takes a reference.
 */
     class FundamentalMatrixComputationExecutor
