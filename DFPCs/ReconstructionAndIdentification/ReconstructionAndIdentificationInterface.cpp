d4b0706f7e8969c10126de1da5235db78cd704a8
Alessandro Bianco <alessandro.bianco@strath.ac.uk>
2018-08-31 10:48:59 +0100

    Adding DFPC

---
+++ DFPCs/ReconstructionAndIdentification/ReconstructionAndIdentificationInterface.cpp
@@ -0,0 +1,59 @@
+/**
+ * @addtogroup DFPCs
+ * @{
+ */
+
+#include "ReconstructionAndIdentificationInterface.hpp"
+
+namespace CDFF
+{
+namespace DFPC
+{
+
+ReconstructionAndIdentificationInterface::ReconstructionAndIdentificationInterface()
+{
+}
+
+ReconstructionAndIdentificationInterface::~ReconstructionAndIdentificationInterface()
+{
+}
+
+void ReconstructionAndIdentificationInterface::leftImageInput(const asn1SccFrame& data)
+{
+    inLeftImage = data;
+}
+
+void ReconstructionAndIdentificationInterface::rightImageInput(const asn1SccFrame& data)
+{
+    inRightImage = data;
+}
+
+void ReconstructionAndIdentificationInterface::modelInput(const asn1SccPointcloud& data)
+{
+    inModel = data;
+}
+
+void ReconstructionAndIdentificationInterface::computeModelFeaturesInput(bool data)
+{
+    inComputeModelFeatures = data;
+}
+
+const asn1SccPointcloud& ReconstructionAndIdentificationInterface::pointCloudOutput() const
+{
+    return outPointCloud;
+}
+
+const asn1SccPose& ReconstructionAndIdentificationInterface::poseOutput() const
+{
+    return outPose;
+}
+
+bool ReconstructionAndIdentificationInterface::successOutput() const
+{
+    return outSuccess;
+}
+
+}
+}
+
+/** @} */
