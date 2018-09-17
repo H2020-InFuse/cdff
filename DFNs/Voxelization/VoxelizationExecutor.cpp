6b15f400a3d30b3242c5e365a8b447efc2e80995
Irene <irene.sanznieto@spaceapplications.com>
2018-09-17 13:35:13 +0200

    Added Voxelization DFN

---
+++ DFNs/Voxelization/VoxelizationExecutor.cpp
@@ -0,0 +1,54 @@
+/**
+ * @addtogroup DFNs
+ * @{
+ */
+
+#include "VoxelizationExecutor.hpp"
+#include <Errors/Assert.hpp>
+
+namespace CDFF
+{
+namespace DFN
+{
+
+//=====================================================================================================================
+VoxelizationExecutor::VoxelizationExecutor(VoxelizationInterface* dfn)
+    : dfn (dfn)
+{
+    ASSERT(dfn != NULL, "VoxelizationExecutor: null dfn in input");
+}
+
+//=====================================================================================================================
+void VoxelizationExecutor::Execute(FrameWrapper::FrameConstPtr inputFrame, asn1SccT_Octree & outputOctree)
+{
+    Execute(*inputFrame, outputOctree);
+}
+
+//=====================================================================================================================
+void VoxelizationExecutor::Execute(FrameWrapper::FrameConstPtr inputFrame, const asn1SccT_Octree * outputOctree)
+{
+    ASSERT(outputOctree != NULL, "VoxelizationExecutor, Calling NO instance creation Executor with a NULL pointer");
+    Execute(*inputFrame, outputOctree);
+}
+
+//=====================================================================================================================
+void VoxelizationExecutor::Execute(const FrameWrapper::Frame& inputFrame, const asn1SccT_Octree * outputOctree)
+{
+    ASSERT( outputOctree == NULL, "VoxelizationExecutor, Calling instance creation executor with a non-NULL pointer");
+    dfn->frameInput(inputFrame);
+    dfn->process();
+    outputOctree = & ( dfn->octreeOutput() );
+}
+
+//=====================================================================================================================
+void VoxelizationExecutor::Execute(const FrameWrapper::Frame& inputFrame, asn1SccT_Octree& outputOctree)
+{
+    dfn->frameInput(inputFrame);
+    dfn->process();
+    outputOctree = dfn->octreeOutput();
+}
+
+}
+}
+
+/** @} */
