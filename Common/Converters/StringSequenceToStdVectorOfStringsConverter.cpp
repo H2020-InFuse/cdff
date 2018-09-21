90f39655cc828eb6f883786f9dc028df27eec777
Irene <irene.sanznieto@spaceapplications.com>
2018-09-21 13:50:05 +0200

    Updated converters to use the new type

--- Common/Converters/StringSequenceToStdVectorOfStringsConverter.cpp
+++ Common/Converters/StringSequenceToStdVectorOfStringsConverter.cpp
@@ -6,7 +6,7 @@
 */
 
 /*!
- * @file StringArrayToStdVectorOfStringsConverter.cpp
+ * @file StringSequenceToStdVectorOfStringsConverter.cpp
  * @date 12/09/2018
  * @author Irene Sanz
  */
@@ -14,7 +14,7 @@
 /*!
  * @addtogroup Converters
  *
- *  This is the class for type conversion from StringArray to std::vector<std::string>.
+ *  This is the class for type conversion from StringSequence to std::vector<std::string>.
  *
  *
  * @{
@@ -28,18 +28,18 @@
  * --------------------------------------------------------------------------
  */
 
-#include "StringArrayToStdVectorOfStringsConverter.hpp"
+#include "StringSequenceToStdVectorOfStringsConverter.hpp"
 #include <Errors/Assert.hpp>
 
 namespace Converters
 {
 
-const std::vector<std::string> StringArrayToStdVectorOfStringsConverter::Convert(const BaseTypesWrapper::T_StringArray& stringArray)
+const std::vector<std::string> StringSequenceToStdVectorOfStringsConverter::Convert(const asn1SccStringSequence& stringSequence)
 {
     std::vector<std::string> strings_vector;
-    for( int index = 0; index < stringArray.nCount; index ++ )
+    for( int index = 0; index < stringSequence.nCount; index ++ )
     {
-        asn1SccT_String string = stringArray.arr[index];
+        asn1SccT_String string = stringSequence.arr[index];
         strings_vector.push_back(std::string (reinterpret_cast<char const*>(string.arr), string.nCount));
     }
     return strings_vector;
