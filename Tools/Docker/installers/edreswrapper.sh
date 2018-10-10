a98f0d5fab73f02dfa3dfc2867c0ac248a009bd5
cbz <clement.bazerque@magellium.fr>
2018-10-10 17:43:53 +0200

    [EVOL] Add edreswrapper installation to dockers

--- Tools/Docker/installers/edreswrapper.sh
+++ Tools/Docker/installers/edreswrapper.sh
@@ -5,38 +5,36 @@
 
 # ## Edres-wrapper 1.0.0 ======================================================
 #
-# Download        http://web.magellium.fr/~H2020INFUSE/edreswrapper-sdk-1.0.0.tar.gz
-# Release notes   ???
+# Download   http://web.magellium.fr/~H2020INFUSE/edreswrapper-sdk-1.0.0.tar.gz
 #
-# Installation    ???
-# Documentation   ???
+# This is a wrapper around the edres library to expose core functions for the 
+# CDFF.
 #
 # ### Dependencies ------------------------------------------------------------
 #
 # Edres-wrapper has the following dependencies:
 #
-# * Required: ...
+# * Required: boost1.53, gsl, jpeg.62, png15, atlas3, opencv2.4.3 
 #
-# * Recommended: ...
-#
-# * Optional: ...
+# The previous libraries have to be bundled with edreswrapper because we don't 
+# have full control on the version used by the edres library. 
 #
 # ### Dependants --------------------------------------------------------------
 #
-# ...
+# CDFF
 
-function install4infuse_edres-wrapper {
+function install4infuse_edreswrapper {
 if [[ ! -d "${INSTALL_DIR}/include/edres-wrapper" ]]; then
 
   # Download source code, extract, and change to resulting directory
-  fetchsource_function edres-wrapper edreswrapper-sdk-1.0.0.tar.gz http://web.magellium.fr/~H2020INFUSE/
+  fetchsource_function edreswrapper edreswrapper-sdk-1.0.0.tar.gz http://web.magellium.fr/~H2020INFUSE/
 
   # Install
   install -m 0644 -D -t "${INSTALL_DIR}/include/edres-wrapper/" include/edres-wrapper/*
   install -m 0644 -D -t "${INSTALL_DIR}/lib/edres-wrapper/" lib/*
-  install -m 0644 -D -t "${INSTALL_DIR}/share/edres-wrapper/cmake/" FindEDRESWRAPPER.cmake
+  install -m 0644 -D -t "${INSTALL_DIR}/share/edreswrapper/" EDRESWRAPPERConfig.cmake
 
   # Remove source directory
-  clean_function edres-wrapper
+  clean_function edreswrapper
 fi
 }
