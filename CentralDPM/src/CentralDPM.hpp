9925580af5439b237300df4b7915e5884078b090
Raul.Dominguez <Raul.Dominguez@dfki.de>
2018-10-11 13:43:47 +0000

    [Central DPM] Directory Structure and Initial Envire Tests

---
+++ CentralDPM/src/CentralDPM.hpp
@@ -0,0 +1,26 @@
+
+/**
+ * @author Raul Dominguez
+ */
+
+#ifndef CENTRAL_DPM_HPP
+#define CENTRAL_DPM_HPP
+
+namespace CDFF
+{
+
+	class CentralDPM
+	{
+		public:
+
+			CentralDPM();
+			virtual ~CentralDPM();
+
+		private:
+
+
+	};
+
+}
+
+#endif // CENTRAL_DPM_HPP
---
+++ CentralDPM/test/initialTest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <envire_core/graph/EnvireGraph.hpp>
+
+using namespace envire::core;
+
+int main() {
+    EnvireGraph g;
+    std::cout << "You instantiated an Envire Graph" << std::endl;
+}
