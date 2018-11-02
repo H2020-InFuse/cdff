b99e8c7eca670cf9aad78804006769fb52fabcf7
Alessandro Bianco <alessandro.bianco@strath.ac.uk>
2018-11-02 12:11:20 +0000

    Fixing leftover DFPCs

--- DFPCs/HapticScanning/HapticScanning.cpp
+++ DFPCs/HapticScanning/HapticScanning.cpp
@@ -5,6 +5,8 @@
 
 #include "HapticScanning.hpp"
 
+#include <Executors/ForceMeshGenerator/ForceMeshGeneratorExecutor.hpp>
+
 namespace CDFF
 {
 namespace DFPC
@@ -23,17 +25,17 @@ HapticScanning::~HapticScanning()
 void HapticScanning::setup()
 {
     configurator.configure(configurationFilePath);
-    InstantiateDFNExecutors();
+    InstantiateDFNs();
 }
 
 void HapticScanning::run()
 {
-    m_force_mesh_generator->Execute(inArmBasePose, inArmEndEffectorPose, inArmEndEffectorWrench, outPointCloud);
+    CDFF::DFN::Executors::Execute(m_force_mesh_generator.get(), inArmBasePose, inArmEndEffectorPose, inArmEndEffectorWrench, outPointCloud);
 }
 
-void HapticScanning::InstantiateDFNExecutors()
+void HapticScanning::InstantiateDFNs()
 {
-    m_force_mesh_generator.reset( new CDFF::DFN::ForceMeshGeneratorExecutor( dynamic_cast<CDFF::DFN::ForceMeshGeneratorInterface*>( configurator.GetDfn("forceMeshGenerator") ) ) );
+    m_force_mesh_generator.reset( dynamic_cast<CDFF::DFN::ForceMeshGeneratorInterface*>( configurator.GetDfn("forceMeshGenerator") ) );
 }
 
 }
