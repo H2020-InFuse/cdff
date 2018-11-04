cfc0661ef1310b49cbbc5c2f0d2124d136e30a7f
Romain Michalec <romain.michalec@strath.ac.uk>
2018-11-04 18:49:11 +0000

    Remove unused includes in the visualizers
    Also basic reformatting

--- Common/Visualizers/OpenCVVisualizer.hpp
+++ Common/Visualizers/OpenCVVisualizer.hpp
@@ -1,118 +1,79 @@
-/* --------------------------------------------------------------------------
-*
-* (C) Copyright …
-*
-* --------------------------------------------------------------------------
-*/
-
-/*!
- * @file OpencvVisualizer.hpp
- * @date 28/02/2018
+/**
  * @author Alessandro Bianco
  */
 
-/*!
+/**
  * @addtogroup Visualizers
- * 
- *  The OpencvVisualizer contains method for the visualization of images and other opencv data structures
- * 
  * @{
  */
 
-#ifndef OPENCV_VISUALIZER_HPP
-#define OPENCV_VISUALIZER_HPP
-
-/* --------------------------------------------------------------------------
- *
- * Includes
- *
- * --------------------------------------------------------------------------
- */
-#include "OpencvVisualizer.hpp"
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/core/core.hpp>
-#include "opencv2/features2d/features2d.hpp"
-#include <stdlib.h>
-#include <string>
+#ifndef OPENCVVISUALIZER_HPP
+#define OPENCVVISUALIZER_HPP
 
 #include <Types/CPP/Frame.hpp>
 #include <Types/CPP/VisualPointFeatureVector2D.hpp>
 #include <Types/CPP/CorrespondenceMap2D.hpp>
-#include <Types/CPP/Pose.hpp>
 #include <Types/CPP/Matrix.hpp>
+#include <Types/CPP/Pose.hpp>
 #include <Converters/FrameToMatConverter.hpp>
+
+#include <opencv2/core/core.hpp>
+
+#include <vector>
+#include <string>
 
 namespace Visualizers
 {
-/* --------------------------------------------------------------------------
- *
- * Class definition
- *
- * --------------------------------------------------------------------------
- */
-class OpencvVisualizer
+	/**
+	 * A class for displaying images and other OpenCV data structures
+	 */
+	class OpencvVisualizer
 	{
-	/* --------------------------------------------------------------------
-	 * Public
-	 * --------------------------------------------------------------------
-	 */
-	public:
-		static void ShowImage(cv::Mat image);
-		static void ShowImage(FrameWrapper::FrameConstPtr frame);
-		static void ShowVisualFeatures(FrameWrapper::FrameConstPtr frame, VisualPointFeatureVector2DWrapper::VisualPointFeatureVector2DConstPtr featuresVector);
-		static void ShowCorrespondences(FrameWrapper::FrameConstPtr frame1, FrameWrapper::FrameConstPtr frame2, CorrespondenceMap2DWrapper::CorrespondenceMap2DConstPtr correspondenceMap);
-		static void ShowQuadrupleCorrespondences(std::vector<FrameWrapper::FrameConstPtr> frameList, std::vector<CorrespondenceMap2DWrapper::CorrespondenceMap2DConstPtr> correspondenceMapList);
-		static void ShowDisparity(cv::Mat disparity);
+		public:
 
-		static void ShowMatrix(MatrixWrapper::Matrix3dConstPtr matrix);
-		static void ShowPose(PoseWrapper::Pose3DConstPtr pose);
+			static void ShowImage(cv::Mat image);
+			static void ShowImage(FrameWrapper::FrameConstPtr frame);
+			static void ShowVisualFeatures(FrameWrapper::FrameConstPtr frame, VisualPointFeatureVector2DWrapper::VisualPointFeatureVector2DConstPtr featuresVector);
+			static void ShowCorrespondences(FrameWrapper::FrameConstPtr frame1, FrameWrapper::FrameConstPtr frame2, CorrespondenceMap2DWrapper::CorrespondenceMap2DConstPtr correspondenceMap);
+			static void ShowQuadrupleCorrespondences(std::vector<FrameWrapper::FrameConstPtr> frameList, std::vector<CorrespondenceMap2DWrapper::CorrespondenceMap2DConstPtr> correspondenceMapList);
+			static void ShowDisparity(cv::Mat disparity);
 
-		static void Enable();
-		static void Disable();
-	/* --------------------------------------------------------------------
-	 * Protected
-	 * --------------------------------------------------------------------
-	 */
-	protected:
-		OpencvVisualizer();
+			static void ShowMatrix(MatrixWrapper::Matrix3dConstPtr matrix);
+			static void ShowPose(PoseWrapper::Pose3DConstPtr pose);
 
-	/* --------------------------------------------------------------------
-	 * Private
-	 * --------------------------------------------------------------------
-	 */
-	private:
-		static const std::string WINDOW_NAME;
-		static bool enabled;
-		static Converters::FrameToMatConverter converter;
+			static void Enable();
+			static void Disable();
+
+		protected:
+
+			OpencvVisualizer();
+
+		private:
+
+			static const std::string WINDOW_NAME;
+			static bool enabled;
+			static Converters::FrameToMatConverter converter;
 	};
 }
 
-/* --------------------------------------------------------------------------
- *
- * Macros definition
- *
- * --------------------------------------------------------------------------
- */
-	#ifndef TESTING
-		#define DEBUG_SHOW_IMAGE(image)
-		#define DEBUG_SHOW_2D_VISUAL_FEATURES(frame, featuresVector)
-		#define DEBUG_SHOW_2D_CORRESPONDENCES(frame1, frame2, correspondenceMap)
-		#define DEBUG_SHOW_QUADRUPLE_2D_CORRESPONDENCES(frameList, correspondenceMapList)
-		#define DEBUG_SHOW_POSE(pose)
-		#define DEBUG_SHOW_MATRIX(matrix)
-		#define DEBUG_SHOW_DISPARITY(disparity)
-	#else
-		#define DEBUG_SHOW_IMAGE(image) Visualizers::OpencvVisualizer::ShowImage(image)
-		#define DEBUG_SHOW_2D_VISUAL_FEATURES(frame, featuresVector) Visualizers::OpencvVisualizer::ShowVisualFeatures(frame, featuresVector)
-		#define DEBUG_SHOW_2D_CORRESPONDENCES(frame1, frame2, correspondenceMap) Visualizers::OpencvVisualizer::ShowCorrespondences(frame1, frame2, correspondenceMap)
-		#define DEBUG_SHOW_QUADRUPLE_2D_CORRESPONDENCES(frameList, correspondenceMapList) Visualizers::OpencvVisualizer::ShowQuadrupleCorrespondences(frameList, correspondenceMapList)
-		#define DEBUG_SHOW_POSE(pose) Visualizers::OpencvVisualizer::ShowPose(pose)
-		#define DEBUG_SHOW_MATRIX(matrix) Visualizers::OpencvVisualizer::ShowMatrix(matrix)
-		#define DEBUG_SHOW_DISPARITY(disparity) Visualizers::OpencvVisualizer::ShowDisparity(disparity)
-	#endif
-
+#ifndef TESTING
+	#define DEBUG_SHOW_IMAGE(image)
+	#define DEBUG_SHOW_2D_VISUAL_FEATURES(frame, featuresVector)
+	#define DEBUG_SHOW_2D_CORRESPONDENCES(frame1, frame2, correspondenceMap)
+	#define DEBUG_SHOW_QUADRUPLE_2D_CORRESPONDENCES(frameList, correspondenceMapList)
+	#define DEBUG_SHOW_POSE(pose)
+	#define DEBUG_SHOW_MATRIX(matrix)
+	#define DEBUG_SHOW_DISPARITY(disparity)
+#else
+	#define DEBUG_SHOW_IMAGE(image) Visualizers::OpencvVisualizer::ShowImage(image)
+	#define DEBUG_SHOW_2D_VISUAL_FEATURES(frame, featuresVector) Visualizers::OpencvVisualizer::ShowVisualFeatures(frame, featuresVector)
+	#define DEBUG_SHOW_2D_CORRESPONDENCES(frame1, frame2, correspondenceMap) Visualizers::OpencvVisualizer::ShowCorrespondences(frame1, frame2, correspondenceMap)
+	#define DEBUG_SHOW_QUADRUPLE_2D_CORRESPONDENCES(frameList, correspondenceMapList) Visualizers::OpencvVisualizer::ShowQuadrupleCorrespondences(frameList, correspondenceMapList)
+	#define DEBUG_SHOW_POSE(pose) Visualizers::OpencvVisualizer::ShowPose(pose)
+	#define DEBUG_SHOW_MATRIX(matrix) Visualizers::OpencvVisualizer::ShowMatrix(matrix)
+	#define DEBUG_SHOW_DISPARITY(disparity) Visualizers::OpencvVisualizer::ShowDisparity(disparity)
 #endif
-/* OpencvVisualizer.hpp */
-/** @} */
 
+#endif // OPENCVVISUALIZER_HPP
+
+/** @} */
--- Common/Visualizers/PCLVisualizer.hpp
+++ Common/Visualizers/PCLVisualizer.hpp
@@ -7,72 +7,68 @@
  * @{
  */
 
-#ifndef PCL_VISUALIZER_HPP
-#define PCL_VISUALIZER_HPP
+#ifndef PCLVISUALIZER_HPP
+#define PCLVISUALIZER_HPP
 
-#include "OpencvVisualizer.hpp"
 #include <Types/CPP/PointCloud.hpp>
 #include <Types/CPP/VisualPointFeatureVector3D.hpp>
 #include <Types/CPP/Pose.hpp>
 
-#include <pcl/io/ply_io.h>
-#include <pcl/visualization/pcl_visualizer.h>
-#include <stdlib.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+
 #include <vector>
+#include <string>
 
 namespace Visualizers
 {
+	/**
+	 * A class for displaying point clouds and other PCL data structures
+	 */
+	class PclVisualizer
+	{
+		public:
 
-/**
- * PclVisualizer: a class for displaying pointclouds
- *                and other PCL data structures
- */
-class PclVisualizer
-{
-	public:
+			static void ShowPointCloud(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud);
+			static void ShowPointClouds(std::vector< pcl::PointCloud<pcl::PointXYZ>::ConstPtr > pointCloudsList);
+			static void ShowPointCloud(PointCloudWrapper::PointCloudConstPtr pointCloud);
+			static void ShowVisualFeatures(PointCloudWrapper::PointCloudConstPtr pointCloud, VisualPointFeatureVector3DWrapper::VisualPointFeatureVector3DConstPtr featuresVector);
+			static void ShowImage(pcl::PointCloud<pcl::RGB>::ConstPtr image);
+			static void ShowPoses(std::vector<PoseWrapper::Pose3D> poseList);
 
-		static void ShowPointCloud(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud);
-		static void ShowPointClouds(std::vector< pcl::PointCloud<pcl::PointXYZ>::ConstPtr > pointCloudsList);
-		static void ShowPointCloud(PointCloudWrapper::PointCloudConstPtr pointCloud);
-		static void ShowVisualFeatures(PointCloudWrapper::PointCloudConstPtr pointCloud, VisualPointFeatureVector3DWrapper::VisualPointFeatureVector3DConstPtr featuresVector);
-		static void ShowImage(pcl::PointCloud<pcl::RGB>::ConstPtr image);
-		static void ShowPoses(std::vector<PoseWrapper::Pose3D> poseList);
+			static void PlacePointCloud(PointCloudWrapper::PointCloudConstPtr sceneCloud, PointCloudWrapper::PointCloudConstPtr objectCloud, PoseWrapper::Pose3DConstPtr objectPoseInScene);
+			static void ShowMatches(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud1, pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud2, const std::vector<int>& indexList1, const std::vector<int>& indexList2);
 
-		static void PlacePointCloud(PointCloudWrapper::PointCloudConstPtr sceneCloud, PointCloudWrapper::PointCloudConstPtr objectCloud, PoseWrapper::Pose3DConstPtr objectPoseInScene);
-		static void ShowMatches(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud1, pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud2, const std::vector<int>& indexList1, 
-			const std::vector<int>& indexList2);
+			static void SavePointCloud(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud, unsigned period = 1);
+			static void SavePointCloud(PointCloudWrapper::PointCloudConstPtr pointCloud, unsigned period = 1);
 
-		static void SavePointCloud(pcl::PointCloud<pcl::PointXYZ>::ConstPtr pointCloud, unsigned period = 1);
-		static void SavePointCloud(PointCloudWrapper::PointCloudConstPtr pointCloud, unsigned period = 1);
+			static void Enable();
+			static void Disable();
+			static void EnableSaving();
+			static void DisableSaving();
 
-		static void Enable();
-		static void Disable();
-		static void EnableSaving();
-		static void DisableSaving();
+		protected:
 
-	protected:
+			PclVisualizer();
 
-		PclVisualizer();
+		private:
 
-	private:
+			struct Color
+			{
+				unsigned r, g, b;
+			};
 
-		struct Color
-		{
-			unsigned r, g, b;
-		};
+			static const std::string WINDOW_NAME;
+			static const std::string SAVE_FILE_BASE_NAME;
+			static const std::string SAVE_FILE_EXTENSION;
+			static const unsigned MAX_POINT_CLOUDS = 10;
+			static const Color COLORS_LIST[MAX_POINT_CLOUDS];
 
-		static const std::string WINDOW_NAME;
-		static const std::string SAVE_FILE_BASE_NAME;
-		static const std::string SAVE_FILE_EXTENSION;
-		static const unsigned MAX_POINT_CLOUDS = 10;
-		static const Color COLORS_LIST[MAX_POINT_CLOUDS];
+			static bool enabled;
+			static bool enabledSaving;
 
-		static bool enabled;
-		static bool enabledSaving;
-
-		static pcl::PointXYZ TransformPoint(pcl::PointXYZ point, PoseWrapper::Transform3DConstPtr transform);
-};
-
+			static pcl::PointXYZ TransformPoint(pcl::PointXYZ point, PoseWrapper::Transform3DConstPtr transform);
+	};
 }
 
 #ifndef TESTING
@@ -97,6 +93,6 @@ class PclVisualizer
 	#define DEBUG_SHOW_POSES(poseList) Visualizers::PclVisualizer::ShowPoses(poseList);
 #endif
 
-#endif // PCL_VISUALIZER_HPP
+#endif // PCLVISUALIZER_HPP
 
 /** @} */
