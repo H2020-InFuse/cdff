81500d33cbb707da99701c39f16fb66bd3e47444
Xavier Martinez <xma@spaceapplications.com>
2018-11-21 18:22:52 +0100

    Fixed Boost install to check all subpackages (in case one package is added later)

--- Tools/Docker/installers/boost.sh
+++ Tools/Docker/installers/boost.sh
@@ -66,15 +66,45 @@
 #   + String_Algo
 
 function install4infuse_boost {
-if [[ ! -d "${INSTALL_DIR}/include/boost" ]]; then
 
+BoostLibs=()
+BoostLibs+=("date_time")
+BoostLibs+=("filesystem")
+BoostLibs+=("iostreams")
+BoostLibs+=("system")
+BoostLibs+=("thread")
+BoostLibs+=("chrono")
+BoostLibs+=("serialization")
+BoostLibs+=("timer")
+BoostLibs+=("program_options")
+BoostLibs+=("atomic2")
+boostLibraries= printf "%s," "${BoostLibs[@]}" | cut -d "," -f 1-${#BoostLibs[@]}
+BoostComplete=true
+
+
+if [[ -d "${INSTALL_DIR}/include/boost" ]]; then
+ echo "Boost Found, checking sub libs..."
+  for i in "${BoostLibs[@]}"
+  do
+    if [[ ! -d "${INSTALL_DIR}/include/boost/"$i ]] ;  then
+      BoostComplete=false
+      echo "missing Boost" $i
+    fi
+  done
+else
+ echo "Boost Not Found."
+ BoostComplete=false
+fi
+
+if [[ $BoostComplete = false ]]; then
+echo "installing"
   # Download source code, extract, and change to resulting directory
   fetchsource_function boost boost_1_66_0.tar.gz https://dl.bintray.com/boostorg/release/1.66.0/source/
 
   # Build and install
   mkdir build
   ./bootstrap.sh \
-    --with-libraries=date_time,filesystem,iostreams,system,thread,chrono,serialization,timer,program_options,atomic \
+    --with-libraries="${boostLibraries}" \
     --prefix="${INSTALL_DIR}"
   ./b2 --build-dir=build -q -j ${CPUS} link=shared install
 
@@ -86,5 +116,7 @@ if [[ ! -d "${INSTALL_DIR}/include/boost" ]]; then
 
   # Remove source and build directories
   clean_function boost
+else
+    echo "Boost is already complete."
 fi
 }
