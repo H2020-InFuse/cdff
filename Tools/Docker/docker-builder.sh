687048d7b58044281a0a63531473570c06fb083e
Xavier Martinez <xma@spaceapplications.com>
2018-12-18 18:02:47 +0100

    Integrate cdff-dev-ci to be build as soon as LABEL is changed.

--- Tools/Docker/docker-builder.sh
+++ Tools/Docker/docker-builder.sh
@@ -7,18 +7,54 @@
 set -e
 
 if [ $# -lt 2 ]; then
-    echo "incorrect parameters: Usage docker-builder.sh [dockerfile] [imagename] [optional=environnement variables]"
+    echo "incorrect parameters: Usage docker-builder.sh [dockerfile] [imagename] [optional=--force] [optional=environnement variables]"
     exit 1
 fi
+
+# will return $op as '<' '=" or '>'
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        op='=';
+        return
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            op='>';
+            return
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            op='<';
+            return
+        fi
+    done
+    op='=';
+    return
+}
 
 IMAGE_NAME=$2
 DOCKER_FILE=$1
 
 #if we passed env variables we pass them along
 ENV_VAR=""
-if ! test -z "$3"
+if ! test -z "$4"
 then
-   ENV_VAR='--build-arg '$3' '
+   ENV_VAR='--build-arg '$4' '
 fi
 
 REGISTRY_PREFIX=nexus.spaceapplications.com/
@@ -27,19 +63,27 @@ IMAGE_TAG=${REGISTRY_PREFIX}${INFUSE_REGISTRY_PREFIX}${IMAGE_NAME}
 
 current_tag=$(grep "LABEL version=" $DOCKER_FILE | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
 latest_tag=$(curl -d 'hook_private_key=ea9dc697-5bc9-4a43-96aa-6257f2fda70e&key='$IMAGE_NAME https://hook.io/datastore/get | tr -d '"')
-echo latest_tag $latest_tag , current_tag  $current_tag
 
-function docker_tag_exists() {
-    curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
-}
+if [ -z "$latest_tag" ]; then
+    latest_tag="0.0"
+fi
 
-if [[ ! $current_tag < $latest_tag ]]; then
-    docker pull $IMAGE_TAG':'$latest_tag || true
-    docker build -t $IMAGE_TAG':'$current_tag -f $DOCKER_FILE $ENV_VAR .
-    docker push $IMAGE_TAG':'$current_tag
-    docker tag $IMAGE_TAG':'$current_tag $IMAGE_TAG':'latest
-    docker push $IMAGE_TAG':'latest
+if [ -z "$current_tag" ]; then
+    current_tag="0.0"
+fi
+
+echo latest_tag $latest_tag
+echo current_tag $current_tag
+vercomp $current_tag $latest_tag
+
+if [[ $op = '>' ]] | [[ $3 == "--force" ]]
+    then
+        docker pull $IMAGE_TAG':'$latest_tag || true
+        docker build -t $IMAGE_TAG':'$current_tag -f $DOCKER_FILE $ENV_VAR .
+        docker push $IMAGE_TAG':'$current_tag
+        docker tag $IMAGE_TAG':'$current_tag $IMAGE_TAG':'latest
+        docker push $IMAGE_TAG':'latest
     curl -d 'hook_private_key=ea9dc697-5bc9-4a43-96aa-6257f2fda70e&key='$IMAGE_NAME'&value='$current_tag https://hook.io/datastore/set
- else 
- echo Image $IMAGE_TAG':'$latest_tag already available on the server.
+    else
+        echo Image $IMAGE_TAG':'$latest_tag already available on the server.
 fi
