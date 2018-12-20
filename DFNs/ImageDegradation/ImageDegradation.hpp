c8088bc0ca03a0b86cabbc5d1dccd38f8c78c08d
rvs <raphael.viards@magellium.fr>
2018-12-20 12:01:14 +0100

    [EVOL] Adding open-source implementation

--- DFNs/ImageDegradation/ImageDegradation.hpp
+++ DFNs/ImageDegradation/ImageDegradation.hpp
@@ -7,6 +7,7 @@
 #define IMAGEDEGRADATION_IMAGEDEGRADATION_HPP
 
 #include "ImageDegradationInterface.hpp"
+#include "Helpers/ParametersListHelper.hpp"
 
 namespace CDFF
 {
@@ -15,7 +16,9 @@ namespace DFN
 namespace ImageDegradation
 {
     /**
-     * TODO Class documentation
+     * @brief Implementation of the resolution degradation algorithms provided by OpenCV
+     * See OpenCV's documentation on cv::resize() for more information
+     * https://docs.opencv.org/3.4.0/da/d54/group__imgproc__transform.html#ga47a974309e9102f5f08231edc7e7529d
      */
     class ImageDegradation : public ImageDegradationInterface
     {
@@ -26,6 +29,31 @@ namespace ImageDegradation
 
             virtual void configure();
             virtual void process();
+
+            struct ImageDegradationParams
+            {
+                /**
+                 * @brief Degradation ratio to be applied over the x-axis
+                 */
+                int xratio;
+
+                /**
+                 * @brief Degradation ratio to be applied over the y-axis
+                 */
+                int yratio;
+
+                /**
+                 * @brief Interpolation method
+                 * See OpenCV's documentation on enum cv::InterpolationFlags for more information
+                 * https://docs.opencv.org/3.4.0/da/d54/group__imgproc__transform.html#ga5bb5a1fea74ea38e1a5445ca803ff121
+                 */
+                int method;
+            };
+
+            Helpers::ParametersListHelper parametersHelper;
+            ImageDegradationParams parameters;
+            static const ImageDegradationParams DEFAULT_PARAMETERS;
+            void ValidateParameters();
     };
 }
 }
