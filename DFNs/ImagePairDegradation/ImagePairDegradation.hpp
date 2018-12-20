57ffd9b9d78d0d0cc1545b418fa57b8768666c4a
Raphaël Viards <raphael.viards@magellium.fr>
2018-12-20 17:12:50 +0100

    [EVOL] Adding open-source implementation

--- DFNs/ImagePairDegradation/ImagePairDegradation.hpp
+++ DFNs/ImagePairDegradation/ImagePairDegradation.hpp
@@ -7,6 +7,7 @@
 #define IMAGEPAIRDEGRADATION_IMAGEPAIRDEGRADATION_HPP
 
 #include "ImagePairDegradationInterface.hpp"
+#include "Helpers/ParametersListHelper.hpp"
 
 namespace CDFF
 {
@@ -15,7 +16,9 @@ namespace DFN
 namespace ImagePairDegradation
 {
     /**
-     * TODO Class documentation
+     * @brief Implementation of the resolution degradation algorithms provided by OpenCV
+     * See OpenCV's documentation on cv::resize() for more information
+     * https://docs.opencv.org/3.4.0/da/d54/group__imgproc__transform.html#ga47a974309e9102f5f08231edc7e7529d
      */
     class ImagePairDegradation : public ImagePairDegradationInterface
     {
@@ -26,6 +29,31 @@ namespace ImagePairDegradation
 
             virtual void configure();
             virtual void process();
+
+            struct ImagePairDegradationParams
+            {
+                /**
+                 * @brief Degradation ratio to be applied over the x-axis
+                 */
+                int xratio;
+
+                /**
+                 * @brief Degradation ratio to be applied over the y-axis
+                 */
+                int yratio;
+
+                /**
+                 * @brief Interpolation method
+                 * See OpenCV's documentation on enum cv::InterpolationFlags for more information
+                 * https://docs.opencv.org/3.4.0/da/d54/group__imgproc__transform.html#ga5bb5a1fea74ea38e1a5445ca803ff121
+                 */
+                int method;
+            };
+
+            Helpers::ParametersListHelper parametersHelper;
+            ImagePairDegradationParams parameters;
+            static const ImagePairDegradationParams DEFAULT_PARAMETERS;
+            void ValidateParameters();
     };
 }
 }
