5ec2fb3a4a057aca070bb0ca071156f0c7599b69
rvs <raphael.viards@magellium.fr>
2018-12-20 11:18:51 +0100

    [EVOL] Adding ImageDegradationEdres implementation

--- DFNs/ImageDegradation/ImageDegradationEdres.cpp
+++ DFNs/ImageDegradation/ImageDegradationEdres.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "ImageDegradationEdres.hpp"
+#include "edres-wrapper/EdresRectification.h"
 
 namespace CDFF
 {
@@ -14,6 +15,12 @@ namespace ImageDegradation
 
 ImageDegradationEdres::ImageDegradationEdres()
 {
+    parametersHelper.AddParameter<int>("ImageDegradationEdresParams", "xratio", parameters.xratio, DEFAULT_PARAMETERS.xratio);
+    parametersHelper.AddParameter<int>("ImageDegradationEdresParams", "yratio", parameters.yratio, DEFAULT_PARAMETERS.yratio);
+    parametersHelper.AddParameter<int>("ImageDegradationEdresParams", "method", parameters.method, DEFAULT_PARAMETERS.method);
+    parametersHelper.AddParameter<int>("ImageDegradationEdresParams", "outType", parameters.outType, DEFAULT_PARAMETERS.outType);
+
+    configurationFilePath = "";
 }
 
 ImageDegradationEdres::~ImageDegradationEdres()
@@ -22,13 +29,31 @@ ImageDegradationEdres::~ImageDegradationEdres()
 
 void ImageDegradationEdres::configure()
 {
-    // TODO DFN configuration steps
+    if(configurationFilePath != ""){
+      parametersHelper.ReadFile(configurationFilePath);
+    }
+    ValidateParameters();
 }
 
 void ImageDegradationEdres::process()
 {
-    // TODO DFN processing steps
+    Edres::degradation(inOriginalImage, outDegradedImage, parameters.xratio, parameters.yratio, static_cast<Edres::DegradationMethod>(parameters.method), static_cast<Edres::PixelDepth>(parameters.outType));
 }
+
+void ImageDegradationEdres::ValidateParameters()
+{
+    ASSERT(parameters.xratio >= 1 && parameters.xratio <= 25, "xratio has to be within [1..25]");
+    ASSERT(parameters.yratio >= 1 && parameters.yratio <= 25, "yratio has to be within [1..25]");
+    ASSERT(parameters.method == 0 || parameters.method == 1, "method has to be 0 or 1");
+    ASSERT(parameters.outType >= 5 && parameters.outType <= 8, "outType has to be within [5..8]");
+}
+
+const ImageDegradationEdres::ImageDegradationEdresParams ImageDegradationEdres::DEFAULT_PARAMETERS = {
+    .xratio = 2,
+    .yratio = 2,
+    .method = 0,
+    .outType = 5
+};
 
 }
 }
