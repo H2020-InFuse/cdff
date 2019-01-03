f1d8b979b60d40bf2720a2f142114430f9db466f
Raphaël Viards <raphael.viards@magellium.fr>
2019-01-03 17:39:46 +0100

    [EVOL] Adding Edres implementation

--- DFNs/StereoRectification/StereoRectificationEdres.cpp
+++ DFNs/StereoRectification/StereoRectificationEdres.cpp
@@ -14,21 +14,70 @@ namespace StereoRectification
 
 StereoRectificationEdres::StereoRectificationEdres()
 {
+    _mapFileLeft = " ";
+    _mapFileRight = " ";
+    _rectification = new Edres::StereoRectification();
+    _initialized = false;
+
+    parametersHelper.AddParameter<std::string>("StereoRectificationEdresParams", "mapFileLeft", parameters.mapFileLeft, DEFAULT_PARAMETERS.mapFileLeft);
+    parametersHelper.AddParameter<std::string>("StereoRectificationEdresParams", "mapFileRight", parameters.mapFileRight, DEFAULT_PARAMETERS.mapFileRight);
+    parametersHelper.AddParameter<int>("StereoRectificationEdresParams", "xratio", parameters.xratio, DEFAULT_PARAMETERS.xratio);
+    parametersHelper.AddParameter<int>("StereoRectificationEdresParams", "yratio", parameters.yratio, DEFAULT_PARAMETERS.yratio);
+    parametersHelper.AddParameter<int>("StereoRectificationEdresParams", "outType", parameters.outType, DEFAULT_PARAMETERS.outType);
+
+    configurationFilePath = "";
 }
 
 StereoRectificationEdres::~StereoRectificationEdres()
 {
+    if( _rectification ){
+        delete _rectification;
+        _rectification = nullptr;
+    }
 }
 
 void StereoRectificationEdres::configure()
 {
-    // TODO DFN configuration steps
+    if(configurationFilePath != ""){
+        parametersHelper.ReadFile(configurationFilePath);
+    }
+    ValidateParameters();
 }
 
 void StereoRectificationEdres::process()
 {
-    // TODO DFN processing steps
+    if( _mapFileLeft != parameters.mapFileLeft ||
+            _mapFileRight != parameters.mapFileRight ){
+        _mapFileLeft = parameters.mapFileLeft;
+        _mapFileRight = parameters.mapFileRight;
+
+        if( !_rectification->init(inOriginalStereoPair.left.data.cols, inOriginalStereoPair.left.data.rows, _mapFileLeft, _mapFileRight)){
+            _initialized = true;
+        }
+        else{
+            _initialized = false;
+        }
+    }
+
+    if(_initialized){
+        (*_rectification)(inOriginalStereoPair, outRectifiedStereoPair, parameters.xratio, parameters.yratio, static_cast<Edres::PixelDepth>(parameters.outType));
+    }
 }
+
+void StereoRectificationEdres::ValidateParameters()
+{
+    ASSERT(parameters.xratio >= 1 && parameters.xratio <= 25, "xratio has to be within [1..25]");
+    ASSERT(parameters.yratio >= 1 && parameters.yratio <= 25, "yratio has to be within [1..25]");
+    ASSERT(parameters.outType >= 5 && parameters.outType <= 8, "outType has to be within [5..8]");
+}
+
+const StereoRectificationEdres::StereoRectificationEdresParams StereoRectificationEdres::DEFAULT_PARAMETERS = {
+    .mapFileLeft = "",
+    .mapFileRight = "",
+    .xratio = 1,
+    .yratio = 1,
+    .outType = 5
+};
 
 }
 }
--- DFNs/StereoRectification/StereoRectificationEdres.hpp
+++ DFNs/StereoRectification/StereoRectificationEdres.hpp
@@ -7,6 +7,8 @@
 #define STEREORECTIFICATION_STEREORECTIFICATIONEDRES_HPP
 
 #include "StereoRectificationInterface.hpp"
+#include "edres-wrapper/EdresRectification.h"
+#include "Helpers/ParametersListHelper.hpp"
 
 namespace CDFF
 {
@@ -15,7 +17,7 @@ namespace DFN
 namespace StereoRectification
 {
     /**
-     * TODO Class documentation
+     * @brief Implementation of the rectification process provided by EDRES library
      */
     class StereoRectificationEdres : public StereoRectificationInterface
     {
@@ -26,6 +28,56 @@ namespace StereoRectification
 
             virtual void configure();
             virtual void process();
+
+            struct StereoRectificationEdresParams
+            {
+                /**
+                 * @brief Path to the correction map files, without filename extension
+                 * There should be two files named "sensor_name.dx" & "sensor_name.dy"
+                 * If these files are located in the directory "path/to/files",
+                 * this parameter should be set to "path/to/files/sensor_name"
+                 */
+                std::string mapFileLeft;
+
+                /**
+                 * @brief Path to the correction map files, without filename extension
+                 * There should be two files named "sensor_name.dx" & "sensor_name.dy"
+                 * If these files are located in the directory "path/to/files",
+                 * this parameter should be set to "path/to/files/sensor_name"
+                 */
+                std::string mapFileRight;
+
+                /**
+                 * @brief Degradation ratio to be applied over the x-axis
+                 */
+                int xratio;
+
+                /**
+                 * @brief Degradation ratio to be applied over the y-axis
+                 */
+                int yratio;
+
+                /**
+                 * @brief Pixel depth of the asn1SccFrame output
+                 * 5 = BYTE: One byte per pixel, Char alignement, 256 levels (8U)
+                 * 6 = INT16: One 16 bits signed integer per pixel (16S)
+                 * 7 = INT32: One 32 bits signed integer per pixel (32S)
+                 * 8 = FLOAT: One float per pixel (32F)
+                 */
+                int outType;
+            };
+
+            Helpers::ParametersListHelper parametersHelper;
+            StereoRectificationEdresParams parameters;
+            static const StereoRectificationEdresParams DEFAULT_PARAMETERS;
+            void ValidateParameters();
+
+        private:
+            Edres::StereoRectification *_rectification;
+            std::string _mapFileLeft;
+            std::string _mapFileRight;
+            bool _initialized;
+
     };
 }
 }
