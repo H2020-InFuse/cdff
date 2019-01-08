21e76137603491ceb1832d661cfe826b9569380e
Vincent Bissonnette <vincent.bissonnette@magellium.fr>
2019-01-08 10:20:27 +0100

    Document StereoSlam interface

--- DFNs/StereoSlam/StereoSlamInterface.hpp
+++ DFNs/StereoSlam/StereoSlamInterface.hpp
@@ -14,6 +14,11 @@ namespace CDFF
 {
 namespace DFN
 {
+/**
+     * @brief DFN that performs visual SLAM on a stereo image pair input.
+     * If tracking is successful, the pose output is relative to the pose
+     * of the first image passed to the tracker.
+     */
     class StereoSlamInterface : public DFNCommonInterface
     {
         public:
@@ -23,14 +28,13 @@ namespace DFN
 
             /**
              * Send value to input port "imagePair"
-             * @param imagePair
-             *     This is the current acquisition
+             * @param imagePair The current image on which to perform tracking.
              */
             virtual void imagePairInput(const asn1SccFramePair& data);
             /**
              * Query value from output port "Pose"
-             * @return Pose
-             *     This is the output transform
+             * @return Pose The latest pose estimated by the SLAM system.
+             * It is expressed relative to the pose of the first image.
              */
             virtual const asn1SccTransformWithCovariance& PoseOutput() const;
 
