dff833698421ec3fa2a1c68889edb6941310ef91
Clément Bazerque <clement.bazerque@magellium.fr>
2019-01-28 11:38:54 +0100

    Move licence from lib to share directory

--- Tools/Docker/installers/edreswrapper.sh
+++ Tools/Docker/installers/edreswrapper.sh
@@ -35,12 +35,12 @@ if [[ ! -d "${INSTALL_DIR}/include/edres-wrapper" ]]; then
     sudo install -m 0644 -D -t "${INSTALL_DIR}/share/edres-wrapper/" Edres-WrapperConfig.cmake
     sudo install -m 0644 -D -t "${INSTALL_DIR}/include/edres-wrapper/" include/edres-wrapper/*.h
     sudo install -m 0644 -D -t "${INSTALL_DIR}/lib/edres-wrapper/" lib/*
-    sudo install -m 0644 -D -t "${INSTALL_DIR}/lib/edres-wrapper/" EDRES_License_Signed_SpaceApps.pdf
+    sudo install -m 0644 -D -t "${INSTALL_DIR}/share/edres-wrapper/" EDRES_License_Signed_SpaceApps.pdf
   else
     install -m 0664 -D -t "${INSTALL_DIR}/share/edres-wrapper/" Edres-WrapperConfig.cmake
     install -m 0664 -D -t "${INSTALL_DIR}/include/edres-wrapper/" include/edres-wrapper/*.h
     install -m 0664 -D -t "${INSTALL_DIR}/lib/edres-wrapper/" lib/*
-    install -m 0664 -D -t "${INSTALL_DIR}/lib/edres-wrapper/" EDRES_License_Signed_SpaceApps.pdf
+    install -m 0664 -D -t "${INSTALL_DIR}/share/edres-wrapper/" EDRES_License_Signed_SpaceApps.pdf
   fi
 
   # Remove extracted library
